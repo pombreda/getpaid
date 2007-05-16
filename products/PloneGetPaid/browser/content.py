569525c6f0d2881ddc77852877274c453f3124c3
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-05-16 18:27:14 +0000

    content integration, make content buyable and edit buyable fields through plone

--- products/PloneGetPaid/browser/content.py
+++ products/PloneGetPaid/browser/content.py
@@ -4,27 +4,149 @@ Content Control
 $Id$
 """
 
-import getpaid.interfaces
-import Products.ContentFlavors.interfaces
+import getpaid.core.interfaces as igetpaid
 
-class ContentControl( object ):
-    """ determines if we can 
+from zope.formlib import form
+from zope import component
+
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.Five.formlib import formbase
+from Products.Five.utilities import marker
+from Products.PloneGetPaid import interfaces
+from base import BaseView, BaseFormView
+
+class PayableFormView( BaseFormView ):
+
+    adapters = None
+    interface = igetpaid.IPayable
+
+    template = ZopeTwoPageTemplateFile('templates/content-template.pt')
+
+    def update( self ):
+        self.payable = component.getMultiAdapter( ( self.context, self.request ),
+                                                    self.interface )
+        self.adapters = { self.interface : self.payable }
+        return super( PayableFormView, self).update()
+        
+    def setUpWidgets( self, ignore_request=False ):
+        self.adapters = self.adapters or {}
+        self.widgets = form.setUpEditWidgets(
+            self.form_fields, self.prefix, self.context, self.request,
+            adapters=self.adapters, ignore_request=ignore_request
+            )
+
+class PayableCreation( PayableFormView, formbase.EditForm ):
+
+    form_fields = form.Fields( igetpaid.IPayable )
+    marker = None
+    
+    def update( self ):
+        # XXX
+        mark( self.context, self.marker )
+        return super( PayableFormView, self).update()        
+        
+class PayableEdit( PayableFormView, formbase.EditForm  ):
+    
+    form_fields = form.Fields( igetpaid.IPayable )
+
+class BuyableCreation( PayableCreation ):
+
+    form_fields = form.Fields( igetpaid.IBuyableContent )
+    interface = igetpaid.IBuyableContent
+    marker = interfaces.IBuyableMarker
+
+class BuyableEdit( PayableEdit ):
+
+    form_fields = form.Fields( igetpaid.IBuyableContent )    
+    interface = igetpaid.IBuyableContent    
+    
+class ShippableCreation( PayableCreation ):
+
+    form_fields = form.Fields( igetpaid.IShippableContent )
+    interface = igetpaid.IShippableContent
+    marker = interfaces.IShippableMarker
+    
+class ShippableEdit( PayableEdit ):
+
+    form_fields = form.Fields( igetpaid.IShippableContent )
+    interface = igetpaid.IShippableContent
+
+class PremiumCreation( PayableCreation ):
+
+    form_fields = form.Fields( igetpaid.IPremiumContent )
+    interface = igetpaid.IPremiumContent
+    marker = interfaces.IPremiumMarker
+    
+class PremiumEdit( PayableEdit ):
+    
+    form_fields = form.Fields( igetpaid.IPremiumContent )
+    interface = igetpaid.IPremiumContent    
+
+
+
+class ContentControl( BrowserView ):
+    """ conditions for presenting various actions
     """
+
+    __allow_access_to_unprotected_subobjects__ = 1
 
     def __init__( self, context, request ):
         self.context = context
         self.request = request
 
-    def allowChangePremiumContent( self ):
-
-        # if its already there then no
-        if getpaid.interfaces.IPremiumContent.providedBy( self.context ):
-            return False
-
-        # must allow for content flavors
-        if not Products.ContentFlavors.interfaces.IFlavorAware( self.context ):
-            return False
-            
         portal = getToolByName( self.context, 'portal_url').getPortalObject()
+        options = interfaces.IGetPaidManagementOptions( portal )
+        self.options = options
+
+    def isBuyable( self ):
+        """ 
+        """
+        return interfaces.IBuyableMarker.providedBy( self.context )
+
+    isBuyable.__roles__ = None
+
+    def isPremium( self ):
+        """
+        """
+        return interfaces.IPremiumMarker.providedBy( self.context )
+
+    isPremium.__roles__ = None
+
+    def isShippable( self ):
+        """
+        """
+        return interfaces.IShippableMarker.providedBy( self.context )
         
+    isShippable.__roles__ = None
+        
+    def allowChangeBuyable( self ):
+        """  
+        """
+        if not self.context.portal_type in self.options.buyable_types:
+            return False
+        elif self.isBuyable() or self.isPremium() or self.isShippable():
+            return False
+        return True
+
+    allowChangeBuyable.__roles__ = None
+    
+    def allowChangeShippable( self ):
+        if not self.context.portal_type in self.options.shippable_types:
+            return False
+        elif self.isBuyable() or self.isPremium() or self.isShippable():
+            return False
+        return True
+    
+    allowChangeShippable.__roles__ = None
+    
+    def allowChangePremiumContent( self ):
+        if not self.context.portal_type in self.options.premium_types:
+            return False
+        elif self.isBuyable() or self.isPremium() or self.isShippable():
+            return False
+        return True
+    allowChangePremiumContent.__roles__ = None
+    
 
