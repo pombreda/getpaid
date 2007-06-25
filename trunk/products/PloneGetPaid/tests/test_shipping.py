1a5f6b09b7e41083495d9ea13006df38b7a93f2a
davidbrenneman <davidbrenneman@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-06-25 21:45:17 +0000

    added stub for testing shippable type

---
+++ trunk/products/PloneGetPaid/tests/test_shipping.py
@@ -0,0 +1,49 @@
+"""Unit tests for setting type as shippable.
+"""
+
+import unittest
+from Testing.ZopeTestCase import ZopeDocTestSuite
+
+from base import PloneGetPaidTestCase
+
+def test_add_to_cart():
+    """Test that payments can be processed.
+    
+    >>> self.setRoles(('Manager',))
+    >>> id = self.portal.invokeFactory('Department', 'dept')
+    >>> id = self.portal.dept.invokeFactory('Employee', 'emp')
+    >>> emp = self.portal.dept.emp
+    
+    Set a title.
+    
+    >>> emp.setTitle('E. M. Ployee')
+    >>> emp.Title()
+    'E. M. Ployee'
+    
+    Set a password (note that the password cannot be read back directly.)
+    
+    >>> emp.setPassword('secret')
+    >>> emp.getPassword()
+    Traceback (most recent call last):
+    ...
+    AttributeError: getPassword
+    
+    >>> emp.setConfirmPassword('secret')
+    >>> emp.getConfirmPassword()
+    Traceback (most recent call last):
+    ...
+    AttributeError: getConfirmPassword
+    
+    Set roles.
+    
+    >>> emp.setRoles(('Reviewer',))
+    >>> emp.getRoles()
+    ('Reviewer',)
+    """
+
+
+
+def test_suite():
+    return unittest.TestSuite((
+            ZopeDocTestSuite(test_class=BorgTestCase),
+        ))
