5f8ce86987a869b28aea073f85ff67a2d5401304
bill.bitranch <bill.bitranch@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-06-29 00:08:50 +0000

    make it generate a googlecheckout button give a shopping cart

--- getpaid.googlecheckout/src/getpaid/googlecheckout/googlecheckout.py
+++ getpaid.googlecheckout/src/getpaid/googlecheckout/googlecheckout.py
@@ -3,8 +3,37 @@
 from zope import component
 from zope import interface
 
-import gchecky.controller as gcontroller
+from gchecky.controller import Controller
+from gchecky import model as gmodel
 
+class GController(gbase.Controller):
+    pass
+
+def gcart_item(entry):
+    return gmodel.item_t(
+        name = entry.name,
+        description = entry.description,
+        unit_price = gmodel.price_t(
+            value = entry.cost,
+            currency = 'USD'
+            ),
+        quantity = entry.quantity
+        )
+
+class GCartAdapter(object):
+    """
+    """
+    def __init__(self, context):
+        self.context = context
+        self.gcart = gmodel.checkout_shopping_cart_t(
+            shopping_cart = gmodel.shopping_cart_t(
+                items = [gcart_item(entry) for entry in cart.values()]
+                )
+            )
+
+    def toxml(self):
+        return self.gcart.toxml()
+        
 
 class GoogleCheckoutProcessor( object ):
    
@@ -14,12 +43,21 @@ class GoogleCheckoutProcessor( object ):
 
     def __init__( self, context ):
         self.context = context
+        self._controller = None
+
+    def getController( self ):
+        if self._controller is None:
+            options = interfaces.IGoogleCheckoutOptions( self.context )
+            self._controller = GController(options.merchant_id,
+                                           options.merchant_key,
+                                           options.server_url == 'Sandbox')
+        return self._controller
+
+    controller = property(getController)
+
+    def cart_post_button( self, cart ):
+        prepared = self.controller.prepare_order(GCartAdapter(cart))
+        return prepared
 
     def authorize( self, order, payment ):
-        options = interfaces.IGoogleCheckoutOptions( self.context )
-
-        controller = gcontroller.Controller(options.merchant_id,
-                                            options.merchant_key,
-                                            options.server_url == 'Test')
-
-        #controller.charge_order()
+        pass
