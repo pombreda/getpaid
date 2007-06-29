8a71c405e8c2110c78473fa55629320792e119e5
ericsteele47 <ericsteele47@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-06-29 16:23:18 +0000

    Adding all config pages. Most still need fields.

--- trunk/products/PloneGetPaid/preferences.py
+++ trunk/products/PloneGetPaid/preferences.py
@@ -12,23 +12,64 @@ import interfaces
 ConfigurationPreferences = PersistentOptions.wire("ConfigurationPreferences",
                                                   "getpaid.configuration",
                                                   interfaces.IGetPaidManagementOptions )
-    
+
+#Profile
 IdentificationPreferences = PersistentOptions.wire("IdentificationPreferences",
                                                   "getpaid.configuration",
-                                                  interfaces.IGetPaidManagementIdentificationOptions )    
+                                                  interfaces.IGetPaidManagementIdentificationOptions)
+#Configure
+ShippableTypesPreferences = PersistentOptions.wire("ShippableTypesPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementShippableTypes )
     
+ShippingOptionsPreferences = PersistentOptions.wire("ShippingOptionsPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementShippingOptions )
+
+PaymentOptionsPreferences = PersistentOptions.wire("PaymentOptionsPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementPaymentOptions )
+#Order Management
+CustomerInformationPreferences = PersistentOptions.wire("CustomerInformationPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementCustomerInformation )
+
+OrderInformationPreferences = PersistentOptions.wire("OrderInformationPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementOrderInformation)
+
+PaymentProcessingPreferences = PersistentOptions.wire("PaymentProcessingPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementPaymentProcessing)
+
+WeightUnitsPreferences = PersistentOptions.wire("WeightUnitsPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementWeightUnits)
+
+SessionTimeoutPreferences = PersistentOptions.wire("SessionTimeoutPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementSessionTimeout)
+
+SalesTaxPreferences = PersistentOptions.wire("SalesTaxPreferences",
+                                                  "getpaid.configuration",
+                                                  interfaces.IGetPaidManagementSalesTaxOptions)
+#Currency
 CurrencyPreferences = PersistentOptions.wire("CurrencyPreferences",
-                                                  "getpaid.configuration",
-                                                  interfaces.IGetPaidManagementCurrencyOptions )    
- 
-MaintenancePreferences = PersistentOptions.wire("MaintenancePreferences",
-                                                  "getpaid.configuration",
-                                                  interfaces.IGetPaidManagementMaintenanceOptions )    
- 
+                                             "getpaid.configuration",
+                                             interfaces.IGetPaidManagementCurrencyOptions )    
+#Emails 
 EmailPreferences = PersistentOptions.wire("EmailPreferences",
-                                                  "getpaid.configuration",
-                                                  interfaces.IGetPaidManagementEmailOptions )    
- 
+                                          "getpaid.configuration",
+                                          interfaces.IGetPaidManagementEmailOptions )    
+
+CustomerNotificationPreferences = PersistentOptions.wire("CustomerNotificationPreferences",
+                                                         "getpaid.configuration",
+                                                         interfaces.IGetPaidManagementCustomerNotificationOptions )                                                     
+
+MerchantNotificationPreferences = PersistentOptions.wire("MerchantNotificationPreferences",
+                                                         "getpaid.configuration",
+                                                         interfaces.IGetPaidManagementMerchantNotificationOptions )   
+#Customize Header/Footer 
 HeaderFooterPreferences = PersistentOptions.wire("HeaderFooterPreferences",
                                                   "getpaid.configuration",
                                                   interfaces.IGetPaidManagementHeaderFooterOptions )    
