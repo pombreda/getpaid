1dc429c19bcb84d1ac2cd7439f137436a279e2d1
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-07-13 06:30:29 +0000

    support for hosting libraries within plonegetpaid, if we have a lib dir magic it when we're loaded onto sys.path

--- products/PloneGetPaid/__init__.py
+++ products/PloneGetPaid/__init__.py
@@ -3,4 +3,12 @@ $Id$
 """
 
 _GETPAID_DEPENDENCIES_ = [ "ore.member", 'CMFonFive' ]
-    
+
+import os, sys
+from Globals import package_home
+
+lib_path = os.path.join( package_home( globals() ), 'lib' )
+if os.path.exists( lib_path ):
+    sys.path.append( lib_path )
+
+
