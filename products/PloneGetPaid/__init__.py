d5382af5cb5a7a40fddab6b966a9ef4bdae63216
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-07-17 18:22:29 +0000

    start on email notifications, also path location needed for admin ui overview

--- products/PloneGetPaid/__init__.py
+++ products/PloneGetPaid/__init__.py
@@ -7,7 +7,8 @@ _GETPAID_DEPENDENCIES_ = [ "ore.member", 'CMFonFive' ]
 import os, sys
 from Globals import package_home
 
-lib_path = os.path.join( package_home( globals() ), 'lib' )
+pkg_home = package_home( globals() )
+lib_path = os.path.join( pkg_home, 'lib' )
 if os.path.exists( lib_path ):
     sys.path.append( lib_path )
 
