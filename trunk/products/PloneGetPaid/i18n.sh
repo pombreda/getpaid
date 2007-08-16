bc11ea278f94e336d96c70740ed5dcc22481aa14
fabione <fabione@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-08-16 22:59:57 +0000

    i18n script and first manual.pot for settings.zcml

---
+++ trunk/products/PloneGetPaid/i18n.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#This script must be executed from the product folder.
+#i18ndude should be available in current $PATH
+
+i18ndude rebuild-pot --pot i18n/plonegetpaid.pot --create i18n/plonegetpaid --merge i18n/manual.pot ./
+
+for file in i18n/plonegetpaid-??.po
+do
+    echo Syncing $file ...
+    i18ndude sync --pot i18n/plonegetpaid.pot $file
+done
+
+#Caveman style extraction of message ids from settings.zcml
+#grep title="" browser/settings.zcml | sed -e "s/.*title=/msgid /g"
+
+#Optional command sequence which prints all the supposedly untranslated strings in page templates
+
+#pagetemplates=`find . -iregex ".*pt"`
+#i18ndude find-untranslated $pagetemplates
