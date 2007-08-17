8a61d0be9886b520df86c17efddb677b2ed4e7f2
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-08-17 21:34:44 +0000

    - Hidding unimplemented options.

--- products/PloneGetPaid/browser/admin.py
+++ products/PloneGetPaid/browser/admin.py
@@ -72,11 +72,13 @@ class ContentTypes( BaseSettingsForm ):
     get paid management interface
     """
     form_fields = form.Fields( interfaces.IGetPaidManagementContentTypes )
-
-    form_fields['buyable_types'].custom_widget = SelectWidgetFactory
+    form_fields = form_fields.omit('buyable_types')
+    form_fields = form_fields.omit('shippable_types')
+    
+    #form_fields['buyable_types'].for_input = False
     form_fields['premium_types'].custom_widget = SelectWidgetFactory
     form_fields['donate_types'].custom_widget = SelectWidgetFactory
-    form_fields['shippable_types'].custom_widget = SelectWidgetFactory
+    #form_fields['shippable_types'].custom_widget = SelectWidgetFactory
 
 
 class ShippingOptions( BaseSettingsForm ):
