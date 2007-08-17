cbaff38818a9e66942aa04f303a60825cdc798a8
mbordese <mbordese@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-08-17 13:20:52 +0000

    fixes for country/state selection widget

--- trunk/products/PloneGetPaid/browser/resources/sequence.js
+++ trunk/products/PloneGetPaid/browser/resources/sequence.js
@@ -1,3 +1,17 @@
+
+inits = new Array();
+
+function addInit(fun){
+    var next = inits.length;
+    inits[next] = fun;
+}
+
+function processInits(){
+    for (i = 0; i < inits.length; i++){
+        inits[i]();
+    }
+}
+
 function getXMLHttpRequest() {
     if (window.XMLHttpRequest){
         ret = new XMLHttpRequest();
@@ -7,8 +21,8 @@ function getXMLHttpRequest() {
     return ret;
 }
 
-
 var http_request = getXMLHttpRequest();
+
 function get_states_from_country(field, field_suffix, dep_suffix){
     var country = field.options[field.selectedIndex].value;
     var url = "@@states-ajax?country=" + country;
@@ -22,10 +36,14 @@ function get_states_from_country(field, field_suffix, dep_suffix){
 function update_sequence(field_id, field_suffix, dep_suffix, response){
     var dep_field_id = field_id.substr(0, field_id.length - field_suffix.length) + dep_suffix;
     var dep_field = document.getElementById(dep_field_id);
+    var dep_value = dep_field.options[dep_field.selectedIndex].value;
 
     var state_options = '';
     for (var i = 0; i < response.length; i++){
-        state_options += "<option value=\"" + response[i][0] + "\">" + response[i][1] + "</option>\n";
+        if (dep_value == response[i][0])
+            state_options += "<option value=\"" + response[i][0] + " selected=\"selected\">" + response[i][1] + "</option>\n";
+        else
+            state_options += "<option value=\"" + response[i][0] + "\">" + response[i][1] + "</option>\n";
     }
 
     dep_field.innerHTML = state_options;
@@ -34,7 +52,6 @@ function update_sequence(field_id, field_suffix, dep_suffix, response){
 function update_callback(field_id, field_suffix, dep_suffix){
    if (http_request.readyState == 4){
         var response = http_request.responseText;
-//         response = [['VAL', 'OPT'], ['VAL2', 'OPT2']];
         response = eval(response);
         update_sequence(field_id, field_suffix, dep_suffix, response);
     }
