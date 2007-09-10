c09ee167e64d6628e42ff5a825f17da22a672483
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-10 22:16:03 +0000

    make it easier to customize notification templates, fix issue 93, bad links in template and order summary

--- products/PloneGetPaid/notifications.py
+++ products/PloneGetPaid/notifications.py
@@ -2,16 +2,17 @@
 email notifications for store admins and customers.
 """
 
-from zope import component
+from zope import component, interface
 from getpaid.core.interfaces import workflow_states
 
 import interfaces
 
 from DocumentTemplate.DT_HTML import HTML
 
+
 customer_new_order_template = HTML('''\
 To: <dtml-var to_email>
-From: "<dtml-var store_name>" <<dtml-var from_email>>
+From: "<dtml-var "store_settings.store_name">" <<dtml-var from_email>>
 Subject: New Order Notification
 
 Thank you for you order.
@@ -20,25 +21,59 @@ Total Amount to be Charged : <dtml-var "order.getTotalPrice()">
 
 You can view the status of your order here
 
-<dtml-var store_url>/@@getpaid-order-history
+<dtml-var store_url>/@@getpaid-order/<dtml-var "order.order_id">
+
+Order Contents
+
+<dtml-in "order.shopping_cart.values()">
+ <dtml-with sequence-item>
+   <dtml-var "resolve().Title()"> <dtml-var cost> <dtml-var quantity>
+  </dtml-with>
+</dtml-in>
 
 ''')
 
+class CustomerOrderNotificationTemplate( object ):
+
+    interface.implements( interfaces.INotificationMailTemplate )
+
+    __call__ = customer_new_order_template
+
+    def __init__( self, order ):
+        self.order = order
+
 merchant_new_order_template = HTML('''\
 To: <dtml-var to_email>
-From: "<dtml-var store_name>" <<dtml-var from_email>>
+From: "<dtml-var "store_settings.store_name">" <<dtml-var from_email>>
 Subject: New Order Notification
 
 A New Order has been created
 
 Total Cost: <dtml-var "order.getTotalPrice()">
 
-The url to continue processing the order is here
-<dtml-var store_url>/@@admin-manage-order/<dtml-var "order.order_id">
+To continue processing the order this order follow this link.
+<dtml-var store_url>/@@admin-manage-order/<dtml-var "order.order_id">/@@admin
+
+Order Contents
+
+<dtml-in "order.shopping_cart.values()">
+ <dtml-with sequence-item>
+   <dtml-var "resolve().Title()"> <dtml-var cost> <dtml-var quantity>
+  </dtml-with>
+</dtml-in>
 
 ''')
-                                   
 
+
+class MerchantOrderNotificationTemplate( object ):
+
+    interface.implements( interfaces.INotificationMailTemplate )
+
+    __call__ = merchant_new_order_template
+                                   
+    def __init__( self, order ):
+        self.order = order
+        
 def sendNotification( order, event ):
     """ sends out email notifications to merchants and clients based on settings.
 
@@ -62,11 +97,13 @@ def sendNotification( order, event ):
     if settings.merchant_email_notification == 'notification' \
        and settings.contact_email:
 
-        message = merchant_new_order_template( to_email = settings.contact_email,
-                                               from_email = settings.contact_email,
-                                               store_name = settings.store_name,
-                                               store_url = store_url,
-                                               order = order)
+        template = component.getAdapter( interfaces.INotificationMailTemplate, order, "merchant-new-order")
+        message = template( to_email = settings.contact_email,
+                            from_email = settings.contact_email,
+                            store_settings = settings,
+                            store_url = store_url,
+                            order = order )
+        
         mailer.send( str(message) )
 
     if settings.customer_email_notification == 'notification' \
@@ -75,11 +112,12 @@ def sendNotification( order, event ):
         member = portal.portal_membership.getMemberById( order.user_id )
 
         if member.getProperty('email'):
-            message = customer_new_order_template( to_email = member.getProperty('email'),
-                                                   from_email = settings.contact_email,
-                                                   store_name = settings.store_name,                                               
-                                                   store_url = store_url,
-                                                   order = order )
+            template = component.getAdapter( interfaces.INotificationMailTemplate, order, "customer-new-order")
+            message = template( to_email = member.getProperty('email'),
+                                from_email = settings.contact_email,
+                                store_settings = settings,
+                                store_url = store_url,
+                                order = order )
             mailer.send( str(message) )
 
     
