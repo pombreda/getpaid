4f54f04f42c7a64de37b735a88258591fa5464aa
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-19 19:08:13 +0000

    - Fixes issue #109, re-instate shipping settings - ignore .pyc files at iso3166 folder

--- products/PloneGetPaid/browser/admin.py
+++ products/PloneGetPaid/browser/admin.py
@@ -67,12 +67,11 @@ class ContentTypes( BaseSettingsForm ):
     """
     form_fields = form.Fields( interfaces.IGetPaidManagementContentTypes )
     form_fields = form_fields.omit('premium_types')
-    form_fields = form_fields.omit('shippable_types')
     
     form_fields['buyable_types'].custom_widget = SelectWidgetFactory
     #form_fields['premium_types'].custom_widget = SelectWidgetFactory
     form_fields['donate_types'].custom_widget = SelectWidgetFactory
-    #form_fields['shippable_types'].custom_widget = SelectWidgetFactory
+    form_fields['shippable_types'].custom_widget = SelectWidgetFactory
 
 
 class ShippingOptions( BaseSettingsForm ):
