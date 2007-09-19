66fba8ee8c6f63418656462fb502ada91b2cb15a
ctxlken <ctxlken@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-19 08:50:26 +0000

    Added Card Holder Name and Phone# to fields that are passed to Authorize.net.  These values now show in the transaction details on Authorize.net, which makes it much easier to track who has made payments.

--- trunk/getpaid.authorizedotnet/src/getpaid/authorizedotnet/authorizenet.py
+++ trunk/getpaid.authorizedotnet/src/getpaid/authorizedotnet/authorizenet.py
@@ -43,6 +43,8 @@ class AuthorizeNetAdapter(object):
         options = dict(
             amount = str(amount),
             card_num = payment.credit_card,
+            last_name = payment.name_on_card,
+            phone     = payment.phone_number,
             exp_date = payment.cc_expiration,
             address = billing.bill_first_line,
             city = billing.bill_city,
