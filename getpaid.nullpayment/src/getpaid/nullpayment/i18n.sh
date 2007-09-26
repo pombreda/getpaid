fe7204f48c265434edf2ee8426c6c5d0cf7ec84e
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-26 21:48:18 +0000

    Incomplete - task 64: Translation i18n tags needed in files http://code.google.com/p/getpaid/issues/detail?id=64 --- getpaid.nullpayment i18nized i18ndude workaround Five locale registration is not working, help hazmat! :)

---
+++ getpaid.nullpayment/src/getpaid/nullpayment/i18n.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#This script must be executed from the product folder.
+#i18ndude should be available in current $PATH
+
+i18ndude rebuild-pot --pot locales/getpaid.nullpayment.pot --create getpaid.nullpayment --merge locales/manual.pot ./
+
+for file in `find locales -name *.po`
+do
+    echo Syncing $file ...
+    i18ndude sync --pot locales/getpaid.nullpayment.pot $file
+done
