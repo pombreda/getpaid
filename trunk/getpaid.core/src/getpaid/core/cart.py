1d66e730a88b1c9f30568cc989d9e4a4d332bbbf
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-27 16:03:23 +0000

    remove old compatibility item

--- trunk/getpaid.core/src/getpaid/core/cart.py
+++ trunk/getpaid.core/src/getpaid/core/cart.py
@@ -8,9 +8,6 @@ from persistent import Persistent
 
 import interfaces
 
-# backwards compatiblity - remove after next release - 7/5/2007
-from item import LineItem
-
 class ShoppingCart( OrderedContainer ):
     """
     A shopping cart
