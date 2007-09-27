c1f426fd93da09343a26040acea7ab4642ccdb75
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-27 14:09:02 +0000

    Incomplete - task 64: Translation i18n tags needed in files http://code.google.com/p/getpaid/issues/detail?id=64 --- Spanish translation Automatic .mo file generation

--- trunk/getpaid.googlecheckout/src/getpaid/googlecheckout/interfaces.py
+++ trunk/getpaid.googlecheckout/src/getpaid/googlecheckout/interfaces.py
@@ -5,6 +5,9 @@ $Id: interfaces.py 10 2007-05-10 15:34:29Z kapilt $
 from getpaid.core import interfaces
 from zope import schema
 
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('getpaid.googlecheckout')
+
 class IGoogleCheckoutProcessor( interfaces.IPaymentProcessor ):
     """
     Google Checkout Processor
@@ -15,9 +18,9 @@ class IGoogleCheckoutOptions( interfaces.IPaymentProcessorOptions ):
     Google Checkout Options
     """
     server_url = schema.Choice(
-        title = u"Google Checkout Server URL",
+        title = _(u"Google Checkout Server URL"),
         values = ( "Sandbox", "Production" ),
         )
 
-    merchant_id = schema.ASCIILine( title = u"Merchant Id")
-    merchant_key = schema.ASCIILine( title = u"Merchant Key")
+    merchant_id = schema.ASCIILine( title = _(u"Merchant Id"))
+    merchant_key = schema.ASCIILine( title = _(u"Merchant Key"))
