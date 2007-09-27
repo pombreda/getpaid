360a891c104eaf79fa57accc93b4d02b44e58de5
jpg.rcom <jpg.rcom@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-09-27 14:48:36 +0000

    Incomplete - task 64: Translation i18n tags needed in files http://code.google.com/p/getpaid/issues/detail?id=64 --- getpaid.paypal begin i18n support

--- getpaid.paypal/src/getpaid/paypal/interfaces.py
+++ getpaid.paypal/src/getpaid/paypal/interfaces.py
@@ -4,6 +4,9 @@
 from getpaid.core import interfaces
 from zope import schema
 
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('getpaid.paypal')
+
 class IPaypalStandardProcessor( interfaces.IPaymentProcessor ):
     """
     Paypal Standard Processor
@@ -14,8 +17,8 @@ class IPaypalStandardOptions( interfaces.IPaymentProcessorOptions ):
     Paypal Standard Options
     """
     server_url = schema.Choice(
-        title = u"Paypal Website Payments Server",
+        title = _(u"Paypal Website Payments Server"),
         values = ( "Sandbox", "Production" ),
         )
 
-    merchant_id = schema.ASCIILine( title = u"Paypal Id")
+    merchant_id = schema.ASCIILine( title = _(u"Paypal Id"))
