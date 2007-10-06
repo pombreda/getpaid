a926b592c0b32acd5647f427aa62b93696f03573
lstask@gmail.com <lstask@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-06 18:47:42 +0000

    added ups module to buildout src and deleted anonymous buildout for quicker checkouts.

---
+++ getpaid.buildout/src/getpaid.ups/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="getpaid.ups",
+    version="0.1",
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['getpaid'],
+    include_package_data=True,
+    install_requires = [ 'setuptools',
+	                 'getpaid.core',
+                         ],
+    zip_safe = False,
+    )
