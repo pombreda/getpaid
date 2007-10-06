ca1c14eeccb9d67d67312a6756d4dab43017d4ff
lstask@gmail.com <lstask@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-06 19:23:46 +0000

    should not have been checked in

--- getpaid.buildout/src/getpaid.ups/src/getpaid/ups/upsconstants.py
+++
@@ -1,9 +0,0 @@
-
-SANDBOX_URL = 'https://wwwcie.ups.com/ups.app/xml/Rate'
-PRODUCTION_URL = 'https://www.ups.com/ups.app/xml/Rate'
-
-TRANSIENT_ERROR = 'TransientError'
-HARD_ERROR = 'HardError'
-WARNING_ERROR = 'Warning'
-
-
