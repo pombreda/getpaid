6f61ddb1fd4eadaab6054a21295ed2121135a7ea
lstask@gmail.com <lstask@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-07 19:37:50 +0000

    now parsing the response back from UPS

--- getpaid.ups/src/getpaid/ups/interfaces.py
+++ getpaid.ups/src/getpaid/ups/interfaces.py
@@ -1,8 +1,7 @@
 
 from getpaid.core import interfaces
 from zope import schema
-#from upsconstants import UPS_SERVICES
-#from widgets import SelectWidgetFactory
+import upsconstants
 
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('getpaid.ups')
@@ -21,20 +20,45 @@ class IUPSRateServiceOptions( interfaces.IShippingRateServiceOptions ):
         values = ( "Sandbox", "Production" ),
         description = _(u"Select Sandbox while testing your store, and switch to Production for the real thing."),
         default = "Sandbox",
+        required = True,
         )
     
-# ups_services = schema.List( title = _(u"UPS Services"),
-#                                                 value_type = schema.Choice( title=u"ups_services", source=vocabulary.SimpleVocabulary.fromValues(UPS_SERVICES) ),
-#                                                 description = _(u"The services to offer in your store."))
+    ups_services = schema.List( title = _(u"UPS Services"),
+                                required = True,
+                                description = _(u"The services to offer in your store."),
+                                value_type = schema.Choice( title=u"ups_services",
+                                vocabulary=schema.vocabulary.SimpleVocabulary.fromValues(upsconstants.UPS_SERVICES.keys() ) ) )
+    
+    ups_account_number = schema.ASCIILine( title = _(u"UPS Account Number"), 
+            required = True,
+            description = _(u"Your six-digit UPS account number."))
     
     ups_username = schema.ASCIILine( title = _(u"UPS User Name"),
-                                    description = _(u"The user name you supplied when registering for your UPS developer key."))
+        required = True,
+        description = _(u"The user name you supplied when registering for your UPS access key."))
                                     
-    ups_password = schema.ASCIILine( title = _(u"UPS Password"),
-                                    description = _(u"The password you supplied when registering for your UPS developer key."))
+    ups_password = schema.Password( title = _(u"UPS Password"),
+        required = True,
+        description = _(u"The password you supplied when registering for your UPS access key."))
                                     
-    ups_developer_key = schema.ASCIILine( title = _(u"UPS Developer Key"), 
-                                            description = _(u"The developer key issued to you by UPS."))
+    ups_access_key = schema.ASCIILine( title = _(u"UPS Access Key"), 
+        required = True,
+        description = _(u"The access (not developer!) key issued to you by UPS."))
+                                            
+    # ups_weight_unit = schema.Choice( title = _(u"Unit of weight"),
+    #     values = upsconstants.UPS_WEIGHT_UNITS.keys(),
+    #     required = True,
+    #     description = _(u"Select which unit of weight to use for your products."),
+    #     default = 'Pounds',
+    #     )
+    #     
+    # ups_currency_code = schema.Choice( title = _(u"UPS Currency Code"),
+    #     values = upsconstants.UPS_CURRENCY_CODES.keys(),
+    #     required = True,
+    #     description = _(u"The currency that UPS will use when calculating your rates."),
+    #     default = 'US Dollars (USD)',
+    #     )
+
 
 
 
--- getpaid.ups/src/getpaid/ups/upsconstants.py
+++ getpaid.ups/src/getpaid/ups/upsconstants.py
@@ -6,8 +6,36 @@ TRANSIENT_ERROR = 'TransientError'
 HARD_ERROR = 'HardError'
 WARNING_ERROR = 'Warning'
 
-UPS_SERVICES =  { "UPS Next Day Air" : "01", 
-                    "UPS Second Day Air" : "02",
-                }
+UPS_SERVICES =  {
+    'UPS Next Day Air' : '01', 
+    'UPS Second Day Air' : '02',
+}
+                
+# UPS_PACKAGING_TYPES = {
+#     ""
+# 
+# }
+
+UPS_PICKUP_TYPES = {
+    'Daily Pickup':'01': ,
+    'Customer Counter':'03',
+}
+
+UPS_RESPONSE_ERRORS = {
+
+
+}
+
+UPS_CURRENCY_CODES = {
+    'US Dollars (USD)' : 'USD',
+    'British Pounds (GBP)' : 'GBP',
+    'Canadian Dollars (CAD)' : 'CAD',
+}
+
+UPS_WEIGHT_UNITS = {
+    'Pounds' : 'LBS',
+    'Kilograms' : 'KGS'
+}
+
 
 
