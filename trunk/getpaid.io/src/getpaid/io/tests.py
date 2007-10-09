70b117c6fbfeb54b55d087bf06aab51f6f4b252d
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-09 04:41:29 +0000

    refactor tests to use new bases in getpaid.core

--- trunk/getpaid.io/src/getpaid/io/tests.py
+++ trunk/getpaid.io/src/getpaid/io/tests.py
@@ -3,51 +3,34 @@
 
 __docformat__ = "reStructuredText"
 
-import doctest
+
 import unittest
 
 from StringIO import StringIO
-
-from zope.testing.doctestunit import DocFileSuite
-from zope.app.testing import placelesssetup, ztapi
-from zope.app.annotation import interfaces as annotation_interfaces
-from zope.app.annotation import attribute
-
-from hurry.workflow import interfaces
-
-import random, string, datetime
+from xml.sax import make_parser
 
 from zope import component
-from xml.sax import make_parser
+from zope.app.testing import ztapi
 from ore.xd import ImportReader
 
 from getpaid.io import writer, reader
-from getpaid.io.interfaces import IStoreWriter, IObjectExportWriter, IObjectImportReader
-from getpaid.core.interfaces import IOrder, IOrderManager, IOrderWorkflowLog, workflow_states
-from getpaid.core import order, cart, item as lineitem
-from getpaid.core.workflow import store, order as oworkflow
+from getpaid.io.interfaces import IObjectExportWriter, IObjectImportReader
 
-class OrderImportTests( unittest.TestCase ):
+from getpaid.core import order
+from getpaid.core.interfaces import IOrder, IOrderManager
+from getpaid.core.tests import base
 
-    def setUp( self ):
-        placelesssetup.setUp()
-        coreSetUp( None )
-        self.orders = list( createOrders() )        
-        self.manager = component.getUtility( IOrderManager )
-                    
-    def tearDown( self ):
-        placelesssetup.tearDown()
-
-class OrderImportExportTests( unittest.TestCase ):
+class OrderImportExportTests( base.GetPaidTestCase ):
 
     def setUp( self ):
-        placelesssetup.setUp()
-        coreSetUp( None )
-        self.orders = list( createOrders() )
+        super( OrderImportExportTests, self).setUp()
+        self.orders = list( base.createOrders() )
         self.manager = component.getUtility( IOrderManager )
+        ztapi.provideAdapter( IOrder, IObjectExportWriter, writer.OrderExportWriter)
+        ztapi.provideAdapter( IOrder, IObjectImportReader, reader.OrderReader)        
         
     def tearDown( self ):
-        placelesssetup.tearDown()
+        super( OrderImportExportTests, self).tearDown()
         self.orders = None
         self.manager = None
         
@@ -81,78 +64,6 @@ class OrderImportExportTests( unittest.TestCase ):
             writer.exportToStream( test_stream )
             self.assertEqual( len(serialized), len(test_stream.getvalue()) )
             
-def createOrders( how_many=10 ):
-    manager = component.getUtility( IOrderManager )
-
-    for i in range(1, how_many):
-        o = order.Order()
-        o.order_id = str(i)
-
-        o.shopping_cart = sc = cart.ShoppingCart()
-
-        for i in range(0, 10):
-            item = lineitem.LineItem()
-            item.name = "p%s"%random.choice( string.letters )
-            item.quantity = random.randint(1,25)
-            item.cost = random.randint(30, 100)
-            item.item_id = "i%s"%random.choice( string.letters )
-            if item.item_id in sc:
-                continue
-            sc[item.item_id] = item
-
-        o.user_id = "u%s"%random.choice( string.letters )
-        o.finance_workflow.fireTransition('create')
-        o.fulfillment_workflow.fireTransition('create')
-
-        manager.store( o )
-
-        yield o
-    
-def coreSetUp(doctest):
-    placelesssetup.setUp()
-
-    ztapi.provideAdapter( IOrder,
-                          interfaces.IWorkflowState,
-                          oworkflow.FinanceState,
-                          'order.finance')
-
-    ztapi.provideAdapter( IOrder,
-                          interfaces.IWorkflowState,
-                          oworkflow.FulfillmentState,
-                          'order.fulfillment')
-
-    ztapi.provideAdapter( IOrder,
-                          interfaces.IWorkflowInfo,
-                          oworkflow.FinanceInfo,
-                          'order.finance')
-
-    ztapi.provideAdapter( IOrder,
-                     interfaces.IWorkflowInfo,
-                     oworkflow.FulfillmentInfo,
-                    'order.fulfillment')
-                    
-    ztapi.provideAdapter(annotation_interfaces.IAttributeAnnotatable,
-                         annotation_interfaces.IAnnotations,
-                         attribute.AttributeAnnotations)
-                         
-    ztapi.provideAdapter( IOrder, IObjectExportWriter, writer.OrderExportWriter)
-    ztapi.provideAdapter( IOrder, IObjectImportReader, reader.OrderReader)
-    
-    ztapi.provideAdapter( IOrder, IOrderWorkflowLog, order.OrderWorkflowLog )
-
-    ztapi.provideUtility(interfaces.IWorkflow,
-                         oworkflow.FulfillmentWorkflow(),
-                        'order.fulfillment')
-                        
-    ztapi.provideUtility(interfaces.IWorkflow,
-                        oworkflow.FinanceWorkflow(),
-                        'order.finance')
-
-    ztapi.provideUtility(interfaces.IWorkflowVersions,
-                         store.StoreVersions())
-    
-    ztapi.provideUtility( IOrderManager, order.OrderManager() )
-    
 
 def test_suite():
     return unittest.TestSuite((
