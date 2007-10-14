fc653f1ab22be285f24e83f7b18a90272f7f3a80
jlenton <jlenton@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-14 14:08:31 +0000

    testing of the install steps, sort of

--- trunk/products/PloneGetPaid/tests/test_setup.py
+++ trunk/products/PloneGetPaid/tests/test_setup.py
@@ -1,12 +1,47 @@
+from zope.app.component.interfaces import ISite
+from zope.app.intid.interfaces import IIntIds
+from zope.component import getUtility
+
+from getpaid.core.interfaces import IOrderManager, IStore
+from Products.PloneGetPaid import _GETPAID_DEPENDENCIES_
+
 from base import PloneGetPaidTestCase
 
 
+
 class TestProductInstall(PloneGetPaidTestCase):
+    # XXX need test for teardown_store (and all uninstallers)
 
     def testNothing(self):
-        pass
-    
+        pass #is this test useful? [chipaca, 2007-10-14]
 
+    def test_portal_is_a_site(self):
+        self.assert_(ISite.providedBy(self.portal))
+
+    def test_portal_is_a_store(self):
+        self.assert_(IStore.providedBy(self.portal))
+
+    def test_order_manager_is_setup(self):
+        sm = self.portal.getSiteManager()
+        self.assert_(list(sm.getUtilitiesFor(IOrderManager)))
+
+    def test_intid_is_setup(self):
+        self.assert_(getUtility(IIntIds, context=self.portal) is not None)
+
+    def test_dependencies_are_installed(self):
+        qi = self.portal.portal_quickinstaller
+        installed = dict.fromkeys([i['id']
+                                   for i in qi.listInstalledProducts()])
+        for dep in _GETPAID_DEPENDENCIES_:
+            self.assert_(dep in installed)
+
+    def test_portlets_are_installed(self):
+        right_slots = self.portal.getProperty('right_slots')
+        if not isinstance( right_slots, str):
+            right_slots = "\n".join(list(right_slots))
+        self.assert_('portlet-shopping-cart' in right_slots)
+        self.assert_('portlet-contentwidget' in right_slots)
+        
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
