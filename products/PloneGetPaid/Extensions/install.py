644420a682aa645de10355f1d127c0ea66cf3986
davide.moro <davide.moro@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-14 10:59:09 +0000

    PloneGetPaid/Extensions/install.py: -Now, we check the current profile and set another setImportContext to the older context (before we assume that is CMFPlone)

--- products/PloneGetPaid/Extensions/install.py
+++ products/PloneGetPaid/Extensions/install.py
@@ -96,11 +96,11 @@ def install( self ):
 
     # Run all import steps for getPaid
     portal = getToolByName(self, 'portal_url').getPortalObject()
-    # TODO: we have to check the current profile and set another setImportContext (now we assume that is CMFPlone)
     setup_tool = getToolByName(portal, 'portal_setup')
+    old_context = setup_tool.getImportContextID()
     setup_tool.setImportContext('profile-Products.PloneGetPaid:default')
     setup_tool.runAllImportSteps()
-    setup_tool.setImportContext('profile-CMFPlone:plone')
+    setup_tool.setImportContext(old_context)
     
     return out.getvalue()
 
