789af83069a53ad57915a821b14b3a3a4473bd57
maurits.vanrees <maurits.vanrees@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-17 14:11:12 +0000

    More BBB for zope(.app).annotation

--- products/PloneGetPaid/__init__.py
+++ products/PloneGetPaid/__init__.py
@@ -14,4 +14,18 @@ lib_path = os.path.join( pkg_home, 'lib' )
 if os.path.exists( lib_path ):
     sys.path.append( lib_path )
 
+# These imports of zope.annotation are needed needed to get
+# annotations to work on Zope 2.9 and 2.10 at the same time.  They are
+# needed in this __init__.py so they are always available as
+# zope.annotation in configure.zcml.
+# See http://comments.gmane.org/gmane.comp.web.zope.general/58334
 
+try:
+    import zope.annotation
+    import zope.annotation.interfaces
+except ImportError:
+    # BBB for Zope 2.9
+    import zope.app.annotation
+    import zope.app.annotation.interfaces
+    sys.modules['zope.annotation'] = zope.app.annotation
+    sys.modules['zope.annotation.interfaces'] = zope.app.annotation.interfaces
