0c1d157cf3c73aae79aab877ee334fd882cf89ec
maurits.vanrees <maurits.vanrees@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-10-19 20:44:47 +0000

    Split Shippable portlet into general base.py and specific ship.py. Other portlets can now inherit from base.py

--- products/PloneGetPaid/browser/portlets/ship.py
+++ products/PloneGetPaid/browser/portlets/ship.py
@@ -1,9 +1,9 @@
 from zope.interface import implements
 from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
-import getpaid.core.interfaces as igetpaid
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.PloneGetPaid.browser.portlets.base import GetPaidRenderer
 from Products.PloneGetPaid.i18n import _
 from Products.PloneGetPaid.interfaces import IShippableMarker
 
@@ -22,34 +22,12 @@ class Assignment(base.Assignment):
         return _(u"Shippable")
 
 
-class Renderer(base.Renderer):
-
-    render = ViewPageTemplateFile('../templates/portlet-content-shippable.pt')
-
-    @property
-    def available(self):
-        return IShippableMarker.providedBy(self.context)
-
-
-    def isPayable(self):
-        """When we are rendered, the context is always payable.
-
-        Or shippable, etcetera.  Otherwise the 'available' method
-        would return False already.
-
-        """
-        return True
-
-    @property
-    def payable(self):
-        """Return the payable (shippable) version of the context.
-
-        Maybe do this the same as in content.py
-        """
-        return igetpaid.IShippableContent(self.context)
-
-
 class AddForm(base.NullAddForm):
 
     def create(self):
         return Assignment()
+
+
+class Renderer(GetPaidRenderer):
+    marker = IShippableMarker
+    render = ViewPageTemplateFile('../templates/portlet-content-shippable.pt')
