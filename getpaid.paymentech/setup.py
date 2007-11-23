8525ba92f14dc91caeffbfe2f050b2ca54c4e460
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-11-23 16:44:08 +0000

    updated the basic skeleton

--- getpaid.paymentech/setup.py
+++ getpaid.paymentech/setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="getpaid.paymentech",
-    version="0.0.1",
+    version="0.1",
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['getpaid'],
     include_package_data=True,
     install_requires = [ 'setuptools',
-                         'zope.interface',
-                         'zope.component',
+                         'getpaid.core'
                          ],
     zip_safe = False,
     )
