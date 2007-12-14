187c30d1fcfd1a00570fb0a65dad750b9aa67c0f
maurits.vanrees <maurits.vanrees@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-12-14 12:22:00 +0000

    pyflakes cleanup

--- trunk/products/PloneGetPaid/member.py
+++ trunk/products/PloneGetPaid/member.py
@@ -1,19 +1,21 @@
-
 from getpaid.core import options, interfaces
 from Products.PloneGetPaid.i18n import _
 from Products.CMFPlone.utils import getSiteEncoding
+
 
 class ContactInfo( options.PropertyBag ):
     title = _("Contact Information")
 
 ContactInfo.initclass( interfaces.IUserContactInformation )
 
+
 def memberContactInformation( user ):
     """
     adapt a member to contact information, based on thier settings
     we assume a default user from a plone site
     """
-    # go from the user to the site via a user containment acquisition ( user, userfolder, container)
+    # go from the user to the site via a user containment acquisition
+    # ( user, userfolder, container)
     store = user.aq_inner.aq_parent.aq_parent
     if not interfaces.IStore.providedBy( store ):
         return None
@@ -23,7 +25,7 @@ def memberContactInformation( user ):
 
     # get contact information for default members from settings
     email = member.getProperty('email')
-    name  = member.getProperty('fullname')
+    name = member.getProperty('fullname')
 
     # get the site encoding
     encoding = getSiteEncoding(store)
@@ -32,4 +34,3 @@ def memberContactInformation( user ):
     info.email = unicode(email, encoding)
     info.name = unicode(name, encoding)
     return info
-
