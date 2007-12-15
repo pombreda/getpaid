1eaf5f81bfe57e971006d5eb42c14728b45f887b
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2007-12-15 17:25:20 +0000

    * updated the state selection widget to have a div container wrapping it so that the javascript replacement finds it  * updated the options we show when there is no javascript enabled (it wasn't bringing back any value or title)  * updated the javascript file to be able to select the previously selected state, refs #161

--- trunk/products/PloneGetPaid/browser/resources/country-states-selection.js
+++ trunk/products/PloneGetPaid/browser/resources/country-states-selection.js
@@ -22,11 +22,12 @@ function update_sequence(field_id, field_suffix, dep_suffix, response){
     var dep_field_id = field_id.substr(0, field_id.length - field_suffix.length) + dep_suffix;
     var dep_container = document.getElementById(dep_field_id  + "_container");
     var dep_field = document.getElementById(dep_field_id);
-    var dep_value = (dep_field.selectedIndex >= 0) ? dep_field.options[dep_field.selectedIndex].value : '';
+    //var dep_value = (dep_field.selectedIndex >= 0) ? dep_field.options[dep_field.selectedIndex].value : '';
 
     var state_options = "<select name=\"" + dep_field_id + "\" id=\"" + dep_field_id + "\">";
     for (var i = 0; i < response.length; i++){
-        if (dep_value == response[i][0]){
+        //if (dep_value == response[i][0]){
+        if (dep_field.selectedIndex == i){
             state_options += "<option value=\"" + response[i][0] + "\" selected>" + response[i][1] + "</option>\n";
         }else{
             state_options += "<option value=\"" + response[i][0] + "\">" + response[i][1] + "</option>\n";
