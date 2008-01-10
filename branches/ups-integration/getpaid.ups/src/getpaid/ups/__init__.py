6f2effb0270ba986b15c8b3952b9e9d3c0adc1d2
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-01-10 20:42:27 +0000

    commit rework of ups integration

--- branches/ups-integration/getpaid.ups/src/getpaid/ups/__init__.py
+++ branches/ups-integration/getpaid.ups/src/getpaid/ups/__init__.py
@@ -1,10 +1,1 @@
-"""
-"""
-from getpaid.core.options import PersistentOptions
-import interfaces
-
-UPSRateServiceOptions = PersistentOptions.wire("UPSRateServiceOptions",
-                                               "getpaid.ups",
-                                               interfaces.IUPSRateServiceOptions )
-
-
+#
