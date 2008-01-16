1c72c13fef0343e6430c1271926d7b190ef3c2fa
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-01-16 14:22:56 +0000

    updated the code to make it be more like an egg

--- trunk/getpaid.discount/setup.py
+++ trunk/getpaid.discount/setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="getpaid.discount",
-    version="0.1",
+    version="0.3",
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['getpaid'],
     include_package_data=True,
-    install_requires = [],
+    install_requires = ['setuptools'],
     zip_safe = False,
     )
