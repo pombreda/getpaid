63398b8c3c5c272f2048ae0ed5cf73da89369a86
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-01-24 15:28:40 +0000

    add a shippable line item type, which records product shipment info

--- getpaid.core/src/getpaid/core/item.py
+++ getpaid.core/src/getpaid/core/item.py
@@ -68,7 +68,16 @@ class LineItem( Persistent ):
     def fulfillment_workflow( self ):
         return IWorkflowInfo( self )
 
+class ShippableLineItem( LineItem ):
     
+    implements( interfaces.IShippableLineItem )
+    weight = ""
+    um_weight = ""
+    um_distance = ""
+    length = ""
+    height = ""
+    width = ""
+
 class PayableLineItem( LineItem ):
     """
     an item in the cart for a payable
@@ -82,4 +91,7 @@ class PayableLineItem( LineItem ):
         utility = component.getUtility( IIntIds )
         return utility.queryObject( self.uid )
         
-        
+class PayableShippableLineItem( ShippableLineItem, PayableLineItem ):
+    """
+    a shippable item in a cart for a payable
+    """
