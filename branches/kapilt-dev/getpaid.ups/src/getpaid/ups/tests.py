2c1b5742f8f06b3c0c4cd15228439327833f3e66
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-01-27 18:16:04 +0000

    refactoring, more unit tests, expanded settings, pull constants into vocabularies

--- branches/kapilt-dev/getpaid.ups/src/getpaid/ups/tests.py
+++ branches/kapilt-dev/getpaid.ups/src/getpaid/ups/tests.py
@@ -10,16 +10,19 @@ from getpaid.ups import rates, interfaces
 from getpaid.core.interfaces import IStoreSettings, IOrder
 
 
+
 class MockStoreSettings( object ):
     interface.implements( IStoreSettings )
     store_name = "Rabbit Furs"
     contact_company = "Furs, LLC"
     contact_name = "Mr. Wolf"
-    contact_phone = "903-213-1012"
-    contact_address = "1126 Alabama St"
+    contact_phone = "5102928662"
+    contact_fax = "9032131012"
+    contact_address = "120 Pierce St."
     contact_address2 = ""
     contact_email = "mrwolf@example.com"
     contact_city = "San Francisco"
+    contact_state = "CA"
     contact_postalcode = "94117"
     contact_country = "US"
                              
@@ -30,11 +33,17 @@ def setUp( test ):
     ztapi.provideUtility( IStoreSettings, MockStoreSettings() )
 
 def test_suite():
+    import os
+    globs = dict( UPS_PASSWORD=os.environ.get('UPS_PASSWORD'),
+                  UPS_USERNAME=os.environ.get('UPS_USERNAME'),
+                  UPS_ACCESS_KEY=os.environ.get('UPS_ACCESS_KEY') )
+    
     return unittest.TestSuite((
         DocFileSuite('readme.txt',
                      setUp=setUp,
                      tearDown=placelesssetup.tearDown,
                      optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS,
+                     globs=globs
                      ),    
         ))
 
