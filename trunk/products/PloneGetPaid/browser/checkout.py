51dc329131c2a5687cea7e31615c4031fce75f08
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-02-21 19:54:46 +0000

    remove order classification, actualize the order markers with additional attributes

--- trunk/products/PloneGetPaid/browser/checkout.py
+++ trunk/products/PloneGetPaid/browser/checkout.py
@@ -442,6 +442,7 @@ class CheckoutReviewAndPay( BaseCheckoutForm ):
         order.order_id = self.wizard.data_manager.get('order_id')
         order.user_id = getSecurityManager().getUser().getId()
         if  self.wizard.data_manager.get('shipping_rate'):
+            order.shipping_service = self.wizard.data_manager.get('shipping_service')
             order.shipping_method = self.wizard.data_manager.get('shipping_rate')
             order.shipping_price = self.wizard.data_manager.get('shipping_price')
         notify( ObjectCreatedEvent( order ) )
