19341ce844d519e1bdc8cd1bcd192b8fa01b893f
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-02-29 21:27:29 +0000

    monkeypatch for five input processing bug

--- products/PloneGetPaid/_patch.py
+++ products/PloneGetPaid/_patch.py
@@ -1,10 +1,20 @@
 """
+
+REQUEST HEADER
+
 original source (zope 2.9.x) didn't match against normalized headers, which caused issues
 between different template engines (z2/z3) stomping over each other's response header.
 
 might still be applicable in zope 2.10
 
 this patch should be filed as bug and go upstream into zope.
+
+
+REQUEST INPUT PROCESSING
+
+five destructively processes list and tuple inputs, when converting to unicode
+so we patch it to only happen once in the request.
+
 """
 
 from ZPublisher.HTTPResponse import HTTPResponse
@@ -22,3 +32,21 @@ def getHeader(self, name, literal=0):
     return self.headers.get(name, None)
 
 HTTPResponse.getHeader = getHeader
+
+from Products.Five.formlib.formbase import FiveFormlibMixin
+from Products.Five.browser import decode
+
+def update( self ):
+    if getattr( self.request, '__inputs_processed', False ):        
+        decode.processInputs( self.request )
+        decode.setPageEncoding( self.request )
+        request.__inputs_processed = True
+    super( FiveFormlibMixin, self).update()
+
+FiveFormlibMixin.update = update
+    
+    
+
+    
+    
+    
