4eaf900efcb27e97444822bb563e1a9fbee08526
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-03-11 00:43:12 +0000

    add admin ui for warehouse management, add plugin installation

--- getpaid.warehouse/src/getpaid/warehouse/interfaces.py
+++ getpaid.warehouse/src/getpaid/warehouse/interfaces.py
@@ -2,20 +2,40 @@
 $Id: $
 """
 
-from zope import interface, schema
+from zope import interface, schema, component
 from getpaid.core.interfaces import IAddress
-
+from zope.app.container.interfaces import IContainer
 from zope.i18nmessageid import MessageFactory
+from zope.viewlet.interfaces import IViewletManager
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema import vocabulary
+
 _ = MessageFactory('getpaid')
 
 class IWarehouse( interface.Interface ):
     name = schema.TextLine( title=_(u"Warehouse Name") )
     location = schema.Object( IAddress )
-    
+
+class IWarehouseContainer( IContainer ):
+    """
+    warehouse container utility
+    """
+
+def WarehouseSource( *args ):
+    container = component.getUtility( IWarehouseContainer )
+    return vocabulary.SimpleVocabulary.fromValues( container.keys() )
+
+interface.directlyProvides( WarehouseSource, IContextSourceBinder )
+
 class IProductInventory( interface.Interface ):   
     pickbin = schema.TextLine( title=_(u"Pick Bin"), description=_("") )
     pallet = schema.TextLine( title=_(u"Pallet"), description=_(""))
     stock  = schema.Int( title=_(u"Quantity in Stock"))
+    warehouse = schema.Choice( title=_(u"Warehouse"), source=WarehouseSource )
     # so the notion is that we automatically adjust this value to give
     # update with incoming orders. stock value would always be manually updated.
     store_stock  = schema.Int( title=_(u"Quantity in Stock, minus existing orders"), )
+
+##
+class IWarehouseContainerVM( IViewletManager ):
+    """ warehouse utility's viewlet manager """
---
+++ getpaid.warehouse/src/getpaid/warehouse/warehouse.py
@@ -0,0 +1,18 @@
+
+from persistent import Persistent
+
+from zope import interface
+from zope.app.container.btree import BTreeContainer
+from zope.app.container.contained import Contained
+
+import interfaces
+
+class WarehouseContainer( BTreeContainer ):
+    
+    interface.implements( interfaces.IWarehouseContainer )
+    
+class Warehouse( Persistent, Contained ):
+
+    interface.implements( interfaces.IWarehouse )
+
+    name = ""
