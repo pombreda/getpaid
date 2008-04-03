ef5773dcfd718c5c4072d753a8cb3867aabf6424
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-04-03 10:45:16 +0000

    reconfigure distutils to getpaid.report

--- trunk/getpaid.report/setup.py
+++ trunk/getpaid.report/setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="getpaid.rdb",
+    name="getpaid.report",
     version="0.1",
-    install_requires=['setuptools'], # use in plone means bad requires spec
+    install_requires=['setuptools', 'SQLAlchemy>0.4'], # use in plone means bad requires spec
     dependency_links=['http://download.zope.org/distribution/',],
     packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
     package_dir= {'':'src'},
@@ -14,7 +14,7 @@ setup(
     zip_safe=False,
     author='Kapil Thangavelu',
     author_email='kapil.foss@gmail.com',
-    description="""Relational Storage For getpaid.core.""",
+    description="""Relational Reports For getpaid.core.""",
     license='X11',
     keywords="zope zope3",
     )
