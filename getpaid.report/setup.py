2f75737e7f752a10279ed089602fec2cc4028f51
kapilt <kapilt@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-04-05 05:16:34 +0000

    add console script entry point for rdb schema setup

--- getpaid.report/setup.py
+++ getpaid.report/setup.py
@@ -3,7 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="getpaid.report",
     version="0.1",
-    install_requires=['setuptools', 'SQLAlchemy>0.4'], # use in plone means bad requires spec
+    author='Kapil Thangavelu',
+    author_email='kapil.foss@gmail.com',
+    description="""Relational Reports For getpaid.core.""",
+    license='X11',
+    keywords="zope zope3",    
+    install_requires=['setuptools', 'getpaid.core', 'getpaid.warehouse', 'SQLAlchemy>0.4'], # use in plone means bad requires spec
     dependency_links=['http://download.zope.org/distribution/',],
     packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
     package_dir= {'':'src'},
@@ -12,9 +17,8 @@ setup(
     '': ['*.txt', '*.zcml', '*.pt'],
     },
     zip_safe=False,
-    author='Kapil Thangavelu',
-    author_email='kapil.foss@gmail.com',
-    description="""Relational Reports For getpaid.core.""",
-    license='X11',
-    keywords="zope zope3",
+    entry_points="""
+    [console_scripts]
+    setup-report-db = getpaid.report.schema:main
+    """
     )
