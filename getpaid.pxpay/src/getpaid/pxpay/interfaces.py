4f511561aa9dcd87645745ebc3ed53fed686907e
darryl.dixon <darryl.dixon@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-05-14 12:04:55 +0000

    Add initial core for PXPay Payment Express hosted payments processor. Functionality is currently that it works for authorising/declining realtime payments, and needs work to integrate it into the normal GetPaid Order Fulfillment process once payment has been approved

---
+++ getpaid.pxpay/src/getpaid/pxpay/interfaces.py
@@ -0,0 +1,32 @@
+from getpaid.core import interfaces
+from zope import schema
+from getpaid.pxpay import _
+
+class IPXPayStandardOptions(interfaces.IPaymentProcessorOptions):
+    """
+    PXPay Standard Options
+    """
+    PxPayServerType = schema.Choice(title = _(u"PXPay Website Payments Server"),
+                                    vocabulary = "getpaid.pxpay.server_urls")
+
+    PxPayUserId = schema.ASCIILine(
+        title = _(u"PXPay UserId"),
+        description = _(u"Enter your PXPay account UserId"),
+        required = True,
+       )
+
+    PxPayKey = schema.ASCIILine(
+        title = _(u"PXPay Key"),
+        description = _(u"Enter the 64 character key that you were supplied for your PXPay account"),
+        required = True,
+      )
+
+    PxPaySiteCurrency = schema.Choice(
+        title = _(u"Site Currency"),
+        vocabulary = "getpaid.pxpay.currencies",
+       )
+
+class IPXPayStandardProcessor(interfaces.IPaymentProcessor):
+    """
+    PXPay Standard Processor
+    """
