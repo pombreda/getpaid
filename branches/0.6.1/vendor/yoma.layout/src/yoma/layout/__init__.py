4a2988c3a209522ff3e80f9cbc9984c60034f742
horacio.duran <horacio.duran@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-05-29 17:16:06 +0000

    This is a new release, it will, I guess, be out at the same time that 0.6 the main goal is to have a release with al the new shiny fixes of trunk, I was thinking of merging trunk with 0.6 but i notice that it may break existing install already using 0.6 (for instance this: "form_fields = CheckoutAddress.form_fields.omit('marketing_preference', 'email_html_format')" ) is not working anymore. So we have a release for those who are happy with current status of 0.6 and another for those who don't mind fixing a little. Release often, release early.

---
+++ branches/0.6.1/vendor/yoma.layout/src/yoma/layout/__init__.py
@@ -0,0 +1,12 @@
+##############################################################################
+#
+# Copyright (c) 2007 YOMA PTY LTD. All Rights Reserved.
+#
+##############################################################################
+"""Layout add-on for zope.formlib
+
+$Id$
+"""
+
+from layout import GridLayout, VertLayout, HorzLayout
+from mixin import LayoutMixin
