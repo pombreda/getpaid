1d5016e7f02f79262fabb90d7a106d8b4b7b4492
horacio.duran <horacio.duran@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-05-29 13:52:02 +0000

    Branch for implementing the optional fields feature at checkout

---
+++ branches/optional-fields-for-checkout/getpaid.pxpay/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="getpaid.pxpay",
+    version="0.1",
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    description = "PXPay payment plugin",
+    license = "GPL",
+    keywords = "getpaid pxpay payment",
+    namespace_packages=['getpaid'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved"],
+    include_package_data=True,
+    install_requires = [ 'setuptools',
+                         'getpaid.core',
+                         'zope.interface',
+                         'zope.component',
+                         'zc.ssl',
+                         'elementtree',
+                         ],
+    zip_safe = False,
+    )
