dcf0d56c27a357a3e6040a3327a84f035b63aa21
calvinhp <calvinhp@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-06-06 17:08:32 +0000

    rolling back r1576 not a good idea for the project

--- products/PloneGetPaid/preferences.py
+++ products/PloneGetPaid/preferences.py
@@ -59,28 +59,3 @@ class DefaultFormSchemas(FormSchemas):
         'payment':BillingInfo,
         }
 
-class CheckoutAddressFormSchemas(FormSchemas):
-
-    interfaces = {
-        'billing_address':core_interfaces.IBillingAddress,
-        'shipping_address':core_interfaces.IShippingAddress,
-        'contact_information':core_interfaces.IUserContactInformation,
-        }
-
-    bags = {
-        'billing_address':BillAddressInfo,
-        'shipping_address':ShipAddressInfo,
-        'contact_information':ContactInfo,
-        }
-
-
-class CheckoutReviewAndPayFormSchemas(FormSchemas):
-
-    interfaces = {
-        'payment':core_interfaces.IUserPaymentInformation,
-        }
-
-    bags = {
-        'payment':BillingInfo,
-        }
-
