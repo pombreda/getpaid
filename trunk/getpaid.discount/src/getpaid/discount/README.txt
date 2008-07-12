8b187d5c8620064acd2d686f5e0c9b6c43bc2850
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-07-12 18:11:05 +0000

    Updated the discount product for 2.5 compatibility:  * removed the skin folder and made it a resource  * moved the portlets around so that we only look at the 2.5 portlets

--- trunk/getpaid.discount/src/getpaid/discount/README.txt
+++ trunk/getpaid.discount/src/getpaid/discount/README.txt
@@ -1,8 +1,11 @@
-Tested with Plone 2.5.5, Plone 3.0.6 and PloneGetPaid 0.6.0.
+Tested with:
+ * Plone 2.5.5 (but not fully working with it, see the note at the bottom),
+ * Plone 3.0.6
+ * PloneGetPaid 0.6.0.
 
 Steps to install:
  * uncomment the getpaid.discount egg into buildout.cfg
- * uncomment the zcml slugs for the getpaid.discount egg into buildout.cfg
+ * if you use Plone3.0, uncomment the zcml slugs for the getpaid.discount egg into buildout.cfg.
  * run bin/buildout
  * start your instance
  * go to portal_setup and run the steps for the profile getpaid.discount
@@ -12,3 +15,7 @@ After that, on each payable product, you will have an available action to make t
 Once you make a product discountable, you will have a new tab "Discountable" available to you, for you to edit/update your discount.
 
 When you make a product discountable, it will automatically show the Discounted Product portlet instead of the default PGP one.
+
+Note: when you use Plone2.5, you can't uncomment the zcml slugs for the getpaid.discount egg into buildout.cfg because it creates an error when you start your instance. I haven't found a fix for that yet. The consequences are that the portlet for the Discounted Product details won't show up. You can probably put the overriding into your own product, and use what I've put into the overrides.zcml as a model.
+
+Developped by Six Feet Up. Contact: lucie AT sixfeetup DOT com, or lucielejard on irc.
--- trunk/getpaid.discount/src/getpaid/discount/browser/portlets/portlets30.py
+++ trunk/getpaid.discount/src/getpaid/discount/browser/portlets/portlets30.py
@@ -1,14 +1,12 @@
-from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 from getpaid.discount.browser.interfaces import IDiscountableMarker
 from getpaid.discount.browser.interfaces import IBuyXGetXFreeableMarker
-from getpaid.discount.browser.interfaces import IDiscountable
-from getpaid.discount.browser.interfaces import IBuyXGetXFreeable
+from getpaid.discount.browser.portlets25 import BaseDiscountPortlet
 
 from zope.interface import implements
-from plone.app.portlets.portlets import base
 from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.portlets import base
 
 from Products.PloneGetPaid.interfaces import PayableMarkerMap
 from Products.PloneGetPaid.interfaces import IPayableMarker
@@ -16,84 +14,6 @@ from Products.PloneGetPaid.browser.portlets import buy
 from Products.PloneGetPaid.browser.portlets import donate
 from Products.PloneGetPaid.browser.portlets import premium
 from Products.PloneGetPaid.browser.portlets import ship
-
-class BaseDiscountPortlet(object):
-    """
-    """
-    def isDiscountable(self):
-        return IDiscountableMarker.providedBy(self.context)
-    
-    def isBuyXGetXFreeable(self):
-        return IBuyXGetXFreeableMarker.providedBy(self.context)
-    
-    def getPrice(self, item):
-        """
-        returns the price of the payable item
-        """
-        for marker, iface in PayableMarkerMap.items():
-            if marker.providedBy(item):
-                payable = iface(item)
-                return payable.price
-        return None
-     
-    def hasNormalDiscount(self, item):
-        """
-        returns a nice display of the discount on the item
-        """
-        result = None
-        price = self.getPrice(item)
-        if IDiscountableMarker.providedBy(item) and price:
-            adapter_obj = IDiscountable(item)
-            discount_value = adapter_obj.getDiscountValue()
-            if discount_value != 0.0:
-                discount_type = adapter_obj.getDiscountType()
-                if discount_type == 'Dollars Off':
-                    result = "$%0.2f ($%0.2f off)" % (price - discount_value, discount_value)
-                else:
-                    result = "$%0.2f (%0.0f%s off)" % (price - price*discount_value/100, discount_value, '%')
-        return result
-
-    def hasBuyXGetXFreeDiscount(self, item):
-        """
-        returns a nice display of the discount on the item
-        """
-        result = None
-        if IBuyXGetXFreeableMarker.providedBy(item):
-            adapter_obj = IBuyXGetXFreeable(item)
-            number_to_buy = adapter_obj.getNumberToBuy()
-            number_free = adapter_obj.getNumberFree()
-            if number_to_buy != 0 and number_free != 0:
-                result = "Order %d get %d additional free" % (number_to_buy, number_free)
-        return result
-    
-
-class DiscountContentPortlet(BrowserView, BaseDiscountPortlet):
-    """ Plone 2.5 View methods for the ContentPortlet """
-    
-    def __init__(self, *args, **kw):
-        super(BrowserView, self).__init__(*args, **kw)
-        
-        
-class PGPContentPortlet(BrowserView):
-    """ Plone 2.5 override of the View methods for the ContentPortlet """
-    payable = None
-    def __init__( self, *args, **kw):
-        super( BrowserView, self).__init__( *args, **kw)
-
-        found = False
-        for marker, iface in PayableMarkerMap.items():
-            if marker.providedBy( self.context ):
-                found = True
-                break
-
-        if found:
-            self.payable = iface( self.context )
-        
-    def isPayable(self):
-        return self.payable is not None and \
-            not IDiscountableMarker.providedBy(self.context) and \
-            not IBuyXGetXFreeableMarker.providedBy(self.context)
-
 
 class DiscountRenderer(base.Renderer, BaseDiscountPortlet):
     """Plone 3.0 Base rendered useful for discount portlets.
@@ -144,7 +64,7 @@ class DiscountableAddForm(base.NullAddForm):
 
 class DiscountableRenderer(DiscountRenderer):
     marker = IDiscountableMarker
-    render = ViewPageTemplateFile('templates/portlet_content_discountable.pt')
+    render = ViewPageTemplateFile('../templates/portlet_content_discountable.pt')
 
 
 class IBuyXGetXfreeablePortlet(IPortletDataProvider):
@@ -169,7 +89,7 @@ class BuyXGetXfreeableAddForm(base.NullAddForm):
 
 class BuyXGetXfreeableRenderer(DiscountRenderer):
     marker = IBuyXGetXFreeableMarker
-    render = ViewPageTemplateFile('templates/portlet_content_buyxgetxfreeable.pt')
+    render = ViewPageTemplateFile('../templates/portlet_content_buyxgetxfreeable.pt')
 
 
 #Plone 3 renderers for the portlets
@@ -212,3 +132,5 @@ class ShipRenderer(ship.Renderer):
         return self.marker.providedBy(self.context) and \
             not IDiscountableMarker.providedBy(self.context) and \
             not IBuyXGetXFreeableMarker.providedBy(self.context)
+
+
