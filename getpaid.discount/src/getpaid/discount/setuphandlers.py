d46eefae5fd27460adb105ddb217ec26f85dc42b
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-07-12 18:11:05 +0000

    Updated the discount product for 2.5 compatibility:  * removed the skin folder and made it a resource  * moved the portlets around so that we only look at the 2.5 portlets

--- getpaid.discount/src/getpaid/discount/setuphandlers.py
+++ getpaid.discount/src/getpaid/discount/setuphandlers.py
@@ -1,43 +1,6 @@
 from StringIO import StringIO
 
-from Products.PloneGetPaid.config import PLONE3
-
-#def install_cart_portlet( self, uninstall=False ):
-#    slot = 'here/@@portlet-shopping-cart/index/macros/portlet'
-#    portal = self.portal_url.getPortalObject()
-#    right_slots = portal.getProperty('right_slots', None)
-#    if right_slots is None:
-#        # Plone 3 does not use left/right_slots so bail out here.
-#        return
-#    if isinstance( right_slots, str):
-#        right_slots = right_slots.split('\n')
-#    else:
-#        right_slots = list( right_slots )
-#    if uninstall:
-#        if slot in right_slots:
-#            right_slots.remove( slot )
-#    else:
-#        if slot not in right_slots:
-#            right_slots.append( slot )
-#    portal._updateProperty( 'right_slots', '\n'.join( right_slots ) )    
-
-#def install_contentwidget_portlet( self, uninstall=False ):
-#    slot = 'here/@@portlet-contentwidget'
-#    portal = self.portal_url.getPortalObject()
-#    right_slots = portal.getProperty('right_slots', None)
-#    if right_slots is None:
-#        return
-#    if isinstance( right_slots, str):
-#        right_slots = right_slots.split('\n')
-#    else:
-#        right_slots = list( right_slots )
-#    if uninstall:
-#        if slot in right_slots:
-#            right_slots.remove( slot )
-#    else:
-#        if slot not in right_slots:
-#            right_slots.append( slot )
-#    portal._updateProperty( 'right_slots', '\n'.join( right_slots ) )    
+from Products.PloneGetPaid.config import PLONE3 
 
 def install_plone3_portlets(self):
     """Adds the discountable portlet to the root of the site
@@ -51,7 +14,7 @@ def install_plone3_portlets(self):
     from zope.app.container.interfaces import INameChooser
     from zope.component import getUtility, getMultiAdapter
     from plone.portlets.interfaces import IPortletManager, IPortletAssignmentMapping
-    from getpaid.discount.browser import portlets
+    from getpaid.discount.browser.portlets import portlets30
 
     # Get some definitions.
     portal = self.portal_url.getPortalObject()
@@ -61,8 +24,8 @@ def install_plone3_portlets(self):
     chooser = INameChooser(manager)
 
     assignments = [
-        portlets.DiscountableAssignment(),
-        portlets.BuyXGetXfreeableAssignment(),
+        portlets30.DiscountableAssignment(),
+        portlets30.BuyXGetXfreeableAssignment(),
         ]
 
     for assignment in assignments:
@@ -81,12 +44,6 @@ def setupVarious(context):
 
     logger = context.getLogger("getpaid.discount-default")
     out = StringIO()
-
-    #print >> out, "Installing Discount Cart Portlet"
-    #install_cart_portlet(site)
-
-    #print >> out, "Installing Discount Content Widget Portlet"
-    #install_contentwidget_portlet(site)
 
     if PLONE3:
         print >> out, "Installing Discount Plone 3 Portlets"
