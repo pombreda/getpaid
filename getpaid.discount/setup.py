f7427d62f007726867132247e6e4b6bb91ee3bc5
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-08-01 22:00:57 +0000

    updated the setup.py file to have more metadata to upload to pypi

--- getpaid.discount/setup.py
+++ getpaid.discount/setup.py
@@ -1,12 +1,55 @@
-from setuptools import setup, find_packages
+# Copyright (c) 2007 ifPeople, Kapil Thangavelu, and Contributors
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
-setup(
-    name="getpaid.discount",
-    version="0.3",
-    packages=find_packages('src'),
-    package_dir={'':'src'},
-    namespace_packages=['getpaid'],
-    include_package_data=True,
-    install_requires = ['setuptools'],
-    zip_safe = False,
-    )
+from setuptools import setup, find_packages
+import os
+
+version = '0.3'
+
+setup(name='itsoc.content',
+      version=version,
+      description="package for plone getpaid handling discounts",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Six Feet Up, Inc.',
+      author_email='info@sixfeetup.com',
+      url='http://code.google.com/p/getpaid',
+      packages=find_packages('src'),
+      namespace_packages=['getpaid'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
