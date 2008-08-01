eed8f144d989cb4a94ecc22e271c65016aac16ff
horacio.duran <horacio.duran@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-08-01 19:36:07 +0000

    Found the way of making the screen show in the config screen

--- getpaid.salesforce/src/getpaid/salesforce/browser/admin.py
+++ getpaid.salesforce/src/getpaid/salesforce/browser/admin.py
@@ -3,7 +3,9 @@ $Id: $
 """
 
 from zope.formlib import form
+from zope import component
 from getpaid.salesforce import  interfaces
+from getpaid.core.options import PersistentOptions
 
 from Products.PloneGetPaid.browser.admin import BaseSettingsForm
 
@@ -17,13 +19,22 @@ class SalesforceScreen( BaseSettingsForm ):
     form_fields = form.Fields(interfaces.ISalesforceOptions)
     form_name = _(u'Salesforce')
 
-    def update( self ):
-        try:
-            interface = iter( self.form_fields ).next().field.interface
-        except StopIteration:
-            interface = None
-        #if interface is not None:
-        #    self.adapters = { interface : interfaces.IGetPaidManagementOptions( self.context ) }
-        super(SalesforceScreen , self).update()
 
+def SalesforceConfigurationPreferences( site ):
+
+    settings = component.queryUtility(interfaces.ISalesforceOptions, site)
+    if settings is None: # we have an unmigrated site.. fallback gracefully
+        return OldConfigurationPreferences( site )
+
+    # store access to the site, because our vocabularies get the setting as context
+    # and want to access portal tools to construct various vocabs
+    settings._v_site = site
+    return settings
+
+# previously we stored settings as annotations on the site, we've migrated this to
+# its own utility, so we don't have to carry context to access the store settings.
+# we have it here so we can do a migration.
+OldConfigurationPreferences = PersistentOptions.wire("OldConfigurationPreferences",
+                                                     "getpaid.salesforce",
+                                                     interfaces.ISalesforceOptions )
 
