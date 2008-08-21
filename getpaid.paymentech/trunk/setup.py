3d34429b8c2d71d41d3f8c28376f7c5c9f1f4f29
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-08-21 20:46:41 +0000

    getpaid.paymentech new version 0.2

--- getpaid.paymentech/trunk/setup.py
+++ getpaid.paymentech/trunk/setup.py
@@ -1,8 +1,26 @@
 from setuptools import setup, find_packages
 
+version = "0.2"
+
 setup(
     name="getpaid.paymentech",
-    version="0.1",
+    version=version,
+    description="package for plone getpaid to hook up the checkout with Paymentech",
+    long_description=open(os.path.join("src", "getpaid", "paymentech", "readme.txt")).read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Framework :: Zope3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Topic :: Office/Business :: Financial"
+        "Topic :: Software Development :: Libraries",
+        ],
+    keywords='',
+    author='Six Feet Up, Inc.',
+    author_email='info@sixfeetup.com',
+    url='http://code.google.com/p/getpaid',
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['getpaid'],
@@ -13,3 +31,4 @@ setup(
                          ],
     zip_safe = False,
     )
+
