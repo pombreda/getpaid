eb17a84d397c4ad559736c1fdd3c05769675cbbb
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-08-29 22:02:56 +0000

    now that we have released the egg to pypi, we increase the version number

--- getpaid.googlecheckout/trunk/setup.py
+++ getpaid.googlecheckout/trunk/setup.py
@@ -16,7 +16,7 @@ def read(*rnames):
 
 setup(
     name='getpaid.googlecheckout',
-    version='0.2dev',
+    version='0.3dev',
     license = 'ZPL2.1',
     author='Getpaid Community',
     author_email='getpaid-dev@googlegroups.com',
