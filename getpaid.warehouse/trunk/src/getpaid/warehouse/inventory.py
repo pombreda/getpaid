712377c7742f625bb594f50b26a54420211b57f4
horacio.duran <horacio.duran@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-09-01 22:41:32 +0000

    Fixed bug that prevents the handler from changing the on hand quantity since it checks onnly if the event.destination was not checking among shippable workflow states but only among buyable ones

--- getpaid.warehouse/trunk/src/getpaid/warehouse/inventory.py
+++ getpaid.warehouse/trunk/src/getpaid/warehouse/inventory.py
@@ -55,10 +55,12 @@ def handleFufilledOrder( order, event ):
     """
     when we fufill an order we decrement, our on hand stock, to come 
     inline with our fufillment of this order.
-    """            
+    """ 
     if not event.destination in ( 
         icore.workflow_states.order.fulfillment.DELIVERED,
-        icore.workflow_states.order.fulfillment.WILL_NOT_DELIVER ):
+        icore.workflow_states.shippable_order.fulfillment.DELIVERED,
+        icore.workflow_states.order.fulfillment.WILL_NOT_DELIVER,
+        icore.workflow_states.shippable_order.fulfillment.WILL_NOT_DELIVER):
         return
             
     for item in order.shopping_cart.values():
@@ -72,6 +74,7 @@ def handleFufilledOrder( order, event ):
             
         inventory = interfaces.IProductInventory( payable )
         inventory.stock -= item.quantity
+        
 
         notify(
             interfaces.InventoryOrderFulfilled( inventory, payable, order, item.quantity )
