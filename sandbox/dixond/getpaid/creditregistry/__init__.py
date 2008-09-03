ddaf19f603459b8e412b2058b9b26840ab6afa1b
darryl.dixon <darryl.dixon@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-09-03 13:47:32 +0000

    Bad import

--- sandbox/dixond/getpaid/creditregistry/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from zope.i18nmessageid import MessageFactory
-creditregistryMessageFactory = MessageFactory('getpaid.creditregistry')
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
