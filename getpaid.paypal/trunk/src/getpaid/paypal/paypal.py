5bdc16df4b7e853311cdf7b41e506625351f19e6
duffyd@kokorice.org <duffyd@kokorice.org@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-09-11 12:35:38 +0000

    Branching code.

--- getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
+++ getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
@@ -21,9 +21,28 @@ class PaypalStandardProcessor( object ):
         self.context = context
 
     def cart_post_button( self, cart ):
+        import pdb;pdb.set_trace()
         options = IPaypalStandardOptions( self.context )
         cartitems = []
         idx = 1
+        button_form = """\
+        <form action="https://%(site)s/cgi-bin/webscr" method="post">
+        <input type="hidden" name="cmd" value="_cart"/>
+        <input type="hidden" name="business" value="%(merchant_id)s"/>
+        <input type="hidden" name="currency_code" value="USD"/>
+        <input type="hidden" name="invoice" value="%(order_number)s"/>
+        %(cart)s
+        <input type="image" src="http://%(site)s/en_US/i/btn/x-click-but01.gif"
+               name="submit"
+               alt="Make payments with PayPal - it's fast, free and secure!"/>
+        </form>"""
+        
+        _button_cart = """
+        <input type="hidden" name="item_name_%(idx)s" value="%(itemname)s"/>
+        <input type="hidden" name="amount_%(idx)s" value="%(amount)s"/>
+        <input type="hidden" name="quantity_%(idx)s" value="%(quantity)s"/>
+        """
+        
         for item in cart.values():
             v = _button_cart % {"idx": idx,
                                 "itemname": item.name,
@@ -31,8 +50,8 @@ class PaypalStandardProcessor( object ):
                                 "quantity": item.quantity,}
             cartitems.append(v)
         formvals = {
-            "site": context.plonegetpaidGetPayPalUrl(),
-            "merchant_id": context.plonegetpaidGetPayPalUserName(),
+            "site": "www.sandbox.paypal.com",
+            "merchant_id": "duffyd@kokorice.org",
             "cart": ''.join(cartitems),
             "order_number": order.order_id,
             }
@@ -40,21 +59,3 @@ class PaypalStandardProcessor( object ):
 
     def authorize( self, order, payment ):
         pass
-
-    _button_form = """\
-    <form action="https://%(site)s/cgi-bin/webscr" method="post">
-    <input type="hidden" name="cmd" value="_cart"/>
-    <input type="hidden" name="business" value="%(merchant_id)s"/>
-    <input type="hidden" name="currency_code" value="USD"/>
-    <input type="hidden" name="invoice" value="%(order_number)s"/>
-    %(cart)s
-    <input type="image" src="http://%(site)s/en_US/i/btn/x-click-but01.gif"
-           name="submit"
-           alt="Make payments with PayPal - it's fast, free and secure!"/>
-    </form>"""
-    
-    _button_cart = """
-    <input type="hidden" name="item_name_%(idx)s" value="%(itemname)s"/>
-    <input type="hidden" name="amount_%(idx)s" value="%(amount)s"/>
-    <input type="hidden" name="quantity_%(idx)s" value="%(quantity)s"/>
-    """
