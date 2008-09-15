7c41c07f50e363ff703140a622383e809977719e
duffyd@kokorice.org <duffyd@kokorice.org@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-09-15 20:04:10 +0000

    Updating setup.py

--- getpaid.paypal/trunk/setup.py
+++ getpaid.paypal/trunk/setup.py
@@ -16,7 +16,7 @@ def read(*rnames):
 
 setup(
     name='getpaid.paypal',
-    version='0.3',
+    version='0.3.1',
     license = 'ZPL2.1',
     author='Getpaid Community',
     author_email='getpaid-dev@googlegroups.com',
