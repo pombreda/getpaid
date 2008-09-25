0cbe24539fbdf60b14df92435e306effd239cbf4
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-09-25 15:45:52 +0000

    commented out some code that seems unfinished, and that was making the getpaid recipe error out. Removed also some extra space

--- vendor/ore.viewlet/trunk/src/ore/viewlet/browser.py
+++ vendor/ore.viewlet/trunk/src/ore/viewlet/browser.py
@@ -1,15 +1,13 @@
-
 try:
     from Products.Five.browser import BrowserView
 except:
     from zope.publisher.browser import BrowserView
 
-class ViewletView( BrowserView ):
-
-
-    def __call__( self ):
+class ViewletView(BrowserView):
+    
+    def __call__(self):
         pass
 
 
-class ViewletViewTraverser( ):
-    pass
+#class ViewletViewTraverser():
+#    pass
