e4585f4aa90033c2cd4f4486071a29db84f16db0
horacio.duran <horacio.duran@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-10-29 20:51:30 +0000

    New tag, need to change externals yetbut this is the easy way

---
+++ vendor/yoma.layout/src/yoma/layout/interfaces.py
@@ -0,0 +1,23 @@
+##############################################################################
+#
+# Copyright (c) 2007 YOMA PTY LTD. All Rights Reserved.
+#
+##############################################################################
+"""layout interfaces
+
+$Id$
+"""
+
+from zope.interface import Interface
+
+
+class ILayout(Interface):
+    pass
+
+
+class IGrid(Interface):
+    pass
+
+
+class ICell(Interface):
+    pass
