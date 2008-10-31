91d49a4fc52f6dc13cd07371cca366f2206f0143
jefroche <jefroche@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-10-31 22:01:12 +0000

    fix ogone check

--- getpaid.ogone/branches/multicurrencyandshippingsupport/src/getpaid/ogone/browser/check.py
+++ getpaid.ogone/branches/multicurrencyandshippingsupport/src/getpaid/ogone/browser/check.py
@@ -1,7 +1,7 @@
 from Products.Five import BrowserView
 from getpaid.core.interfaces import IOrderManager
-from getpaid.ogone.interfaces import IOgoneStandardOptions
 from zope.component import getUtility
+
 
 class OgoneCheck(BrowserView):
     """
@@ -14,11 +14,12 @@ class OgoneCheck(BrowserView):
         the XML to Ogone
         """
         orderId = self.request.orderID
-        options = IOgoneStandardOptions(self.context)
-        currency = options.currency
         orderManager = getUtility(IOrderManager)
         order = orderManager.get(orderId)
-        ogonePrice = int(order.getTotalPrice() * 100)
+        shippingType = order.deliveryInformations.shipping
+        countryId = order.countryId
+        ogonePrice = int(order.getTotalPrice(countryId,
+                                             shippingType) * 100)
         return '<orderid="%s" amount="%s" currency="%s">' % (order.getOrderId(),
                                                              ogonePrice,
-                                                             currency)
+                                                             order.currencyId)
