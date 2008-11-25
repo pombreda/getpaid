3ebb48d9f8e3457230b899d822cae5e3cf6fca2a
james4765@gmail.com <james4765@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-11-25 16:51:31 +0000

    Add capability to choose currency in shopping cart code, and enable round-tripping back from paypal

--- getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
+++ getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
@@ -6,6 +6,8 @@ from zope import interface
 from interfaces import IPaypalStandardOptions
 from interfaces import IPaypalStandardProcessor
 
+from Products.CMFCore.utils import getToolByName
+
 _sites = {
     "Sandbox": "www.sandbox.paypal.com",
     "Production": "www.paypal.com",
@@ -24,22 +26,20 @@ class PaypalStandardProcessor( object ):
         options = IPaypalStandardOptions( self.context )
         cartitems = []
         idx = 1
-        _button_form = """
-        <form style="display:inline;" action="https://%(site)s/cgi-bin/webscr" method="post">
-        <input type="hidden" name="cmd" value="_cart"/>
-        <input type="hidden" name="upload" value="1">
-        <input type="hidden" name="business" value="%(merchant_id)s"/>
-        <input type="hidden" name="currency_code" value="USD"/>
-        %(cart)s
-        <input type="image" src="http://%(site)s/en_US/i/btn/x-click-but01.gif"
-               name="submit"
-               alt="Make payments with PayPal - it's fast, free and secure!"/>
-        </form>"""
-        _button_cart = """
-        <input type="hidden" name="item_name_%(idx)s" value="%(itemname)s"/>
-        <input type="hidden" name="amount_%(idx)s" value="%(amount)s"/>
-        <input type="hidden" name="quantity_%(idx)s" value="%(quantity)s"/>
-        """
+        _button_form = """<form style="display:inline;" action="https://%(site)s/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_cart" />
+<input type="hidden" name="upload" value="1" />
+<input type="hidden" name="business" value="%(merchant_id)s" />
+<input type="hidden" name="currency_code" value="%(currency)s" />
+<input type="hidden" name="return" value="%(return_url)s" />
+%(cart)s
+<input type="image" src="http://%(site)s/en_US/i/btn/x-click-but01.gif"
+    name="submit"
+    alt="Make payments with PayPal - it's fast, free and secure!" />
+</form>"""
+        _button_cart = """<input type="hidden" name="item_name_%(idx)s" value="%(itemname)s" />
+<input type="hidden" name="amount_%(idx)s" value="%(amount)s" />
+<input type="hidden" name="quantity_%(idx)s" value="%(quantity)s" />"""
         
         for item in cart.values():
             v = _button_cart % {"idx": idx,
@@ -51,6 +51,8 @@ class PaypalStandardProcessor( object ):
             "site": _sites[options.server_url],
             "merchant_id": options.merchant_id,
             "cart": ''.join(cartitems),
+            "return_url": "%s/@@getpaid-thank-you" % getToolByName(self.context, "portal_url").getPortalObject().absolute_url(),
+            "currency": options.currency,
             }
         return _button_form % formvals
 
