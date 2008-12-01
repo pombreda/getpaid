0bab8b783178a5f99b54d1292340dc2eed9f9911
james4765@gmail.com <james4765@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-12-01 21:15:30 +0000

    Add cart saving capability - still needs IPN functionality to finish full getpaid transaction

--- getpaid.paypal/trunk/src/getpaid/paypal/browser/cart.py
+++ getpaid.paypal/trunk/src/getpaid/paypal/browser/cart.py
@@ -1,18 +1,14 @@
 from Products.PloneGetPaid.browser.cart import ShoppingCartActions
-from Products.PloneGetPaid.interfaces import IGetPaidManagementOptions
-from getpaid.core.interfaces import IPaymentProcessor, IShoppingCartUtility
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from zope.component import getAdapter, getUtility
+from zope.component import getUtility
 
 class Actions(ShoppingCartActions):
 
     template = ZopeTwoPageTemplateFile('templates/cart-actions.pt')
     
     def render(self):
-        cart_utility = getUtility(IShoppingCartUtility)
         html = self.template()
-        cart_utility.destroy(self.context)
         return html
 
     def actionsOtherThanCheckout(self):
@@ -24,8 +20,10 @@ class Actions(ShoppingCartActions):
     
     def buyNowButton(self):
         portal = getToolByName(self.context, 'portal_url').getPortalObject()
-        cart = getUtility(IShoppingCartUtility).get(self.context, create=True)
-        manage_options = IGetPaidManagementOptions(portal)
-        processor_name = manage_options.payment_processor
-        processor = getAdapter(portal, IPaymentProcessor, processor_name)
-        return processor.cart_post_button(cart)
+        button = """<form action='%s' style="display:inline;">
+<input type="submit" class="button context"
+    id="getpaid-paypal-button" name="paynow" value="Pay Now" 
+    i18n:attributes="value" />
+</form>"""
+        url = "%s/@@getpaid-paypal-redirect" % portal.absolute_url()
+        return button % url
--- getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
+++ getpaid.paypal/trunk/src/getpaid/paypal/paypal.py
@@ -7,6 +7,7 @@ from interfaces import IPaypalStandardOptions
 from interfaces import IPaypalStandardProcessor
 
 from Products.CMFCore.utils import getToolByName
+from getpaid.core import interfaces as GetPaidInterfaces
 
 _sites = {
     "Sandbox": "www.sandbox.paypal.com",
@@ -22,11 +23,11 @@ class PaypalStandardProcessor( object ):
     def __init__( self, context ):
         self.context = context
 
-    def cart_post_button( self, cart ):
+    def cart_post_button( self, order ):
         options = IPaypalStandardOptions( self.context )
         cartitems = []
         idx = 1
-        _button_form = """<form style="display:inline;" action="https://%(site)s/cgi-bin/webscr" method="post">
+        _button_form = """<form style="display:inline;" action="https://%(site)s/cgi-bin/webscr" method="post" id="paypal-button">
 <input type="hidden" name="cmd" value="_cart" />
 <input type="hidden" name="upload" value="1" />
 <input type="hidden" name="business" value="%(merchant_id)s" />
@@ -41,20 +42,25 @@ class PaypalStandardProcessor( object ):
 <input type="hidden" name="amount_%(idx)s" value="%(amount)s" />
 <input type="hidden" name="quantity_%(idx)s" value="%(quantity)s" />"""
         
-        for item in cart.values():
+        for item in order.shopping_cart.values():
             v = _button_cart % {"idx": idx,
                                 "itemname": item.name,
                                 "amount": item.cost,
                                 "quantity": item.quantity,}
             cartitems.append(v)
+        siteURL = getToolByName(self.context, "portal_url").getPortalObject().absolute_url()
         formvals = {
             "site": _sites[options.server_url],
             "merchant_id": options.merchant_id,
             "cart": ''.join(cartitems),
-            "return_url": "%s/@@getpaid-thank-you" % getToolByName(self.context, "portal_url").getPortalObject().absolute_url(),
+            "return_url": "%s/@@getpaid-thank-you?order_id=%s" % (siteURL, order.order_id),
             "currency": options.currency,
             }
         return _button_form % formvals
+    
+    def capture(self, order, price):
+        # always returns async - just here to make the processor happy
+        return GetPaidInterfaces.keys.results_async
 
     def authorize( self, order, payment ):
         pass
