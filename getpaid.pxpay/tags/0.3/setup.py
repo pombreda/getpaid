4846e07b93df1578198a04d6385bc81f4db9e615
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2008-12-31 14:48:27 +0000

    removed dev from the version number

--- getpaid.pxpay/tags/0.3/setup.py
+++ getpaid.pxpay/tags/0.3/setup.py
@@ -17,7 +17,7 @@ def read(*rnames):
 
 setup(
     name="getpaid.pxpay",
-    version="0.3dev",
+    version="0.3",
     author='getpaid community',
     author_email='getpaid-dev@googlegroups.com',
     classifiers=[
