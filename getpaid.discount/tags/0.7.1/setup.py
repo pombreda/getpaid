8b6ca9d371c8a158ff704f6fb313080fe0b13679
lucielejard <lucielejard@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-01-28 18:54:10 +0000

    removed dev from the version number

--- getpaid.discount/tags/0.7.1/setup.py
+++ getpaid.discount/tags/0.7.1/setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.7.1dev'
+version = '0.7.1'
 
 setup(name='getpaid.discount',
       version=version,
