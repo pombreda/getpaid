2f79717b4d0e2915049048b058b84e91a1fcd8b0
larubbio <larubbio@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-02-12 00:23:43 +0000

    Add getpaid.formgen into the VariableDonationAmounts branch

---
+++ getpaid.formgen/branches/VariableDonationAmounts/setup.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='getpaid.formgen',
+      version=version,
+      description=u"PloneGetPaid♡PloneFormGen integration".encode('utf-8'),
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("src", "docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)"
+        ],
+      keywords='plone plonegetpaid ploneformgen',
+      author='Daniel Holth',
+      author_email='daniel.holth@exac.com',
+      url='http://dingoskidneys.com/cgi-bin/hgwebdir.cgi/getpaid.formgen',
+      packages=find_packages('src'),
+      package_dir={'':'src'},
+      namespace_packages=['getpaid'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.DataGridField >= 1.6rc1',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
