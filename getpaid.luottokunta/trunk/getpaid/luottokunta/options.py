6950db8041846722dfb26810f72d3c507cbd3b70
taito.horiuchi <taito.horiuchi@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-05-04 16:56:44 +0000

    two files added

---
+++ getpaid.luottokunta/trunk/getpaid/luottokunta/options.py
@@ -0,0 +1,9 @@
+"""
+"""
+
+from getpaid.core.options import PersistentOptions
+import interfaces
+
+LuottokuntaOptions = PersistentOptions.wire("Luottokunta",
+                                               "getpaid.luottokunta",
+                                               interfaces.ILuottokuntaOptions )
