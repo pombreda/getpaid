945d9cc4d4d1789745331a01f5f8394f4ff310eb
mikko@redinnovation.com <mikko@redinnovation.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-06-29 00:04:52 +0000

    Nullpayment now 100% functional with multiple payment processor support

---
+++ getpaid.nullpayment/branches/multiplepaymentprocessors/src/getpaid/nullpayment/admin.py
@@ -0,0 +1,15 @@
+"""
+
+    Site setup screens contributed by this payment processor.
+
+"""
+
+# Views we are going to subclass are imported from here
+from Products.PloneGetPaid.browser import admin as base
+
+class PaymentProcessor(base.PaymentProcessor):
+    """ Simple wrapper to render payment processor settings screen using a certain payment processor """
+
+    def getPaymentProcessorName(self):
+        # Match getpaid.core.interfaces.IPaymentProcessor name
+        return "Testing Processor"
