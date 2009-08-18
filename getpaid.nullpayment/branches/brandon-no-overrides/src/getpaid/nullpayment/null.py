9160a3565833108ff7783ef3fbf55109de5f3e3a
brandon.craig.rhodes <brandon.craig.rhodes@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-08-18 16:35:27 +0000

    Made the null payment processor capable of providing a (dummy) checkout link.

--- getpaid.nullpayment/branches/brandon-no-overrides/src/getpaid/nullpayment/null.py
+++ getpaid.nullpayment/branches/brandon-no-overrides/src/getpaid/nullpayment/null.py
@@ -48,6 +48,9 @@ class NullPaymentAdapter( object ):
         self.context = context
         self.settings = INullPaymentOptions( self.context )
         
+    def checkout_link(self):
+        return u'<b>[ Null Payment Processor Button ]</b>'
+
     def authorize( self, order, payment ):
         if self.settings.allow_authorization == u'allow_authorization':
             annotation = IAnnotations( order )
