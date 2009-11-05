6b8f2a65778e66a63ddab9f797d40d8d03352ca9
hannesc <hannesc@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-11-05 04:59:23 +0000

    0.1 Tag for getpaid.netcash

---
+++ getpaid.netcash/tags/0.1/setup.py
@@ -0,0 +1,42 @@
+"""
+$Id: setup.py 1868 2008-08-22 22:00:38Z fairwinds.dp $
+
+Copyright (c) 2007 - 2008 ifPeople, Kapil Thangavelu, and Contributors
+All rights reserved. Refer to LICENSE.txt for details of distribution and use.
+
+Distutils setup
+ 
+"""
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='getpaid.netcash',
+    version='0.1',
+    license = 'ZPL2.1',
+    author='Hannes Calitz',
+    author_email='hannes@opennetworks.co.za',
+    description='Get paid NetCash payment processor functionality. Built on the VCS processor.',
+    long_description = (),
+    classifiers = [
+        'Framework :: Zope2',
+        'Framework :: Zope3',
+        'Framework :: Plone'
+        ],
+    url='http://code.google.com/p/getpaid',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    namespace_packages=['getpaid'],
+    include_package_data=True,
+    install_requires = ['setuptools',
+                        'getpaid.core',
+                        'zope.interface',
+                        'zope.component',
+                       ],
+    zip_safe = False,
+    )
+
