d5e2afbccfa79c02cb89121be54902e6ca8e34f3
yuejun.tang <yuejun.tang@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2009-11-12 09:19:15 +0000

    add i18n function

--- getpaid.discount/trunk/src/getpaid/discount/browser/interfaces.py
+++ getpaid.discount/trunk/src/getpaid/discount/browser/interfaces.py
@@ -1,18 +1,18 @@
 from zope import schema
 from zope.interface import Interface
-
+from getpaid.discount import discountMessageFactory as _
 class IDiscountable(Interface):
     """
     Marker interface for the Discountable items
     """
-    discount_title = schema.TextLine(title=u'Discount Title',
+    discount_title = schema.TextLine(title=_(u'Discount Title'),
                            required=True)
     
-    discount_type = schema.Choice(title=u'Type of Discount',
-                                  values=['Dollars Off', 'Percentage Off'],
+    discount_type = schema.Choice(title=_(u'Type of Discount'),
+                                  values=[_(u'Dollars Off'), _(u'Percentage Off')],
                                   required=True)
     
-    discount_value = schema.Float(title=u'Value of the discount',
+    discount_value = schema.Float(title=_(u'Value of the discount'),
                                   required=True,)
 
 class IDiscountableMarker(Interface):
@@ -24,13 +24,13 @@ class IBuyXGetXFreeable(Interface):
     """
     Marker interface for the BuyXGetXFreeable items
     """
-    discount_title = schema.TextLine(title=u'Discount Title',
+    discount_title = schema.TextLine(title=_(u'Discount Title'),
                            required=True)
     
-    number_to_buy = schema.Int(title=u'Number of items to buy',
+    number_to_buy = schema.Int(title=_(u'Number of items to buy'),
                                   required=True)
     
-    number_free = schema.Int(title=u'Number of free items',
+    number_free = schema.Int(title=_(u'Number of free items'),
                                   required=True)
 
 class IBuyXGetXFreeableMarker(Interface):
@@ -42,13 +42,13 @@ class ICodeDiscountable(Interface):
     """
     Marker interface for the CodeDiscountable items
     """
-    discount_title = schema.TextLine(title=u'Discount Title',
+    discount_title = schema.TextLine(title=_(u'Discount Title'),
                            required=True)
 
-    discount_code = schema.TextLine(title=u'Discount Code',
+    discount_code = schema.TextLine(title=_(u'Discount Code'),
                            required=True)
     
-    discounted_price = schema.Float(title=u'Price after Discount',
+    discounted_price = schema.Float(title=_(u'Price after Discount'),
                                   required=True,)
 
 
