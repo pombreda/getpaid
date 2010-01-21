8a4975be64f3087878168d612f9c2a1ac31ac864
taito.horiuchi <taito.horiuchi@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-01-21 09:34:47 +0000

    delete MAC branch

--- getpaid.verkkomaksut/branches/MAC/getpaid/verkkomaksut/__init__.py
+++
@@ -1,5 +0,0 @@
-from zope.i18nmessageid import MessageFactory
-VerkkomaksutMessageFactory = MessageFactory('getpaid.verkkomaksut')
-
-#def initialize(context):
-#    """Initializer called when used as a Zope 2 product."""
