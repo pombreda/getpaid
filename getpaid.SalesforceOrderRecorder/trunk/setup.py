719d955600b8b31703b324060c04ddd6a9d9a1f7
dglick <dglick@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-01-27 06:17:12 +0000

    package cleanup and prep for release

--- getpaid.SalesforceOrderRecorder/trunk/setup.py
+++ getpaid.SalesforceOrderRecorder/trunk/setup.py
@@ -5,18 +5,17 @@ version = open(os.path.join("getpaid", "SalesforceOrderRecorder", "version.txt")
 
 setup(name='getpaid.SalesforceOrderRecorder',
       version=version,
-      description="GetPaid plugin allowing for storage or orders in Salesforce.com",
-      long_description=open(os.path.join("getpaid", "SalesforceOrderRecorder", "README.txt")).read(),
+      description="GetPaid plugin allowing for recording orders in Salesforce.com",
+      long_description=open("README.txt").read() + "\n" + open("CHANGES.txt").read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Framework :: Zope2",
         "Framework :: Plone",
         ],
       keywords='Zope CMF Plone Salesforce.com CRM integration',
       author='Rob LaRubbio',
-      author_email='rob@onenw.org',
+      author_email='larubbio@gmail.com',
       url='http://code.google.com/p/getpaid',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
