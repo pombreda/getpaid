a254baa7a5f42b1bd46fe24a1def390334ace601
dglick <dglick@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-04-07 22:08:47 +0000

    version bump

--- getpaid.wizard/trunk/setup.py
+++ getpaid.wizard/trunk/setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 setup(
     name="getpaid.wizard",
-    version="0.4",
+    version="0.5dev",
     install_requires=['setuptools',
                       'getpaid.core',], 
     dependency_links=['http://download.zope.org/distribution/',],
