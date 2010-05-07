b43099395c831ea2b429a0a3af97a6867f81c902
fulviocasali <fulviocasali@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-05-07 19:00:37 +0000

    Added descriptions to the "Payable" form for recurring items.

--- getpaid.core/branches/seattle-recurring-payment/src/getpaid/core/interfaces.py
+++ getpaid.core/branches/seattle-recurring-payment/src/getpaid/core/interfaces.py
@@ -264,8 +264,10 @@ class IShippableContent( IPayable ):
 class IRecurringPaymentContent( IPayable ):
     """ Recurring Payable Content
     """
-    frequency = schema.TextLine( title = _(u"Frequency"))
-    total_occurrences = schema.TextLine( title = _(u"Total Occurrences"))
+    frequency = schema.TextLine( title = _(u"Interval"),
+                                 description = _(u"Use the value 1 for monthly payments, 12 for yearly payments, 3 for quarterly payments, or any other interval you prefer.") )
+    total_occurrences = schema.TextLine( title = _(u"Total Occurrences"),
+                                         description = _(u"The subscription will end after this many payments.") )
 
 #################################
 # Events
