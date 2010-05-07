ae9474c5629e84de6112e871d9c2b8d8999b050c
jesses@npowerseattle.org <jesses@npowerseattle.org@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-05-07 01:20:51 +0000

    basic recurring payment processingimplementation

--- getpaid.paypal/branches/seattle-recurring-payment/src/getpaid/paypal/paypal.py
+++ getpaid.paypal/branches/seattle-recurring-payment/src/getpaid/paypal/paypal.py
@@ -2,8 +2,6 @@
 """
 import urllib
 
-from Products.CMFCore.utils import getToolByName
-from zope import component
 from zope import interface
 from zope.app.component.hooks import getSite
 
@@ -47,10 +45,55 @@ class PaypalBaseButton(object):
     
 class PaypalRecurringButton(PaypalBaseButton):
     
+    unit_token_map = {'months':'M',}
+    
     def __call__(self):
         """ return a properly assembled button for recurring payments
         """
-        return "<h1>Hi, I'm a recurring payment button</h1>"
+        
+        _button_form = """<form style="display:inline;" action="https://%(site)s/cgi-bin/webscr" method="post" id="paypal-button">
+<input value="_xclick-subscriptions" name="cmd" type="hidden" />
+<input name="a3" type="hidden" value="%(cost)s"/>
+<input name="p3" type="hidden" value="%(frequency)s"/>
+<input name="t3" type="hidden" value="%(unit)s"/>
+<input name="srt" type="hidden" value="%(total_occurrences)s"/>
+<input name="item_name" type="hidden" value="%(item_name)%" />
+<input name="src" type="hidden" value="1" />
+<input name="sra" type="hidden" value="1" />
+<input type="hidden" name="upload" value="1" />
+<input type="hidden" name="business" value="%(merchant_id)s" />
+<input type="hidden" name="currency_code" value="%(currency)s" />
+<input type="hidden" name="return" value="%(return_url)s" />
+<input type="hidden" name="cbt" value="Return to %(store_name)s" />
+<input type="hidden" name="rm" value="2" />
+<input type="hidden" name="notify_url" value="%(IPN_url)s" />
+<input type="hidden" name="invoice" value="%(order_id)s" />
+<input type="hidden" name="no_note" value="1" />
+<input type="image" src="http://%(site)s/en_US/i/btn/x-click-but01.gif"
+    name="submit"
+    alt="Make payments with PayPal - it's fast, free and secure!" />
+</form>
+"""
+
+        item = self.order.shopping_cart.values()[0]
+        formvals = {
+            "site": _sites[self.options.server_url],
+            "merchant_id": self.options.merchant_id,
+            "return_url": self.return_url,
+            "currency": self.options.currency,
+            "IPN_url" : self.ipn_url,
+            "order_id" : self.order.order_id,
+            "store_name": self.manage_options.store_name,
+            "item_name": item.name,
+            "item_number" : item.product_code,
+            "cost": item.cost,
+            "quantity": item.quantity,
+            "unit": self.unit_token_map[item.unit],
+            "frequency": item.frequency,
+            "total_occurrences": item.total_occurrences,
+            }
+            
+        return _button_form % formvals
     
 class PaypalStandardButton(PaypalBaseButton):
     
@@ -81,7 +124,6 @@ class PaypalStandardButton(PaypalBaseButton):
 <input type="hidden" name="amount_%(idx)s" value="%(amount)s" />
 <input type="hidden" name="quantity_%(idx)s" value="%(quantity)s" />
 """
-        
         for index, item in enumerate(self.order.shopping_cart.values()):
             # last-minute sanity check for mixed recurring and non-recurring items
             if IRecurringLineItem.providedBy(item):
