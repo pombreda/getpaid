7ecfe3c4e1c0a4c5e5b2de6090d6afdaf9294a0f
datakurre <datakurre@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2010-07-30 08:18:58 +0000

    Fixed dummy authorize-method.

--- getpaid.verkkomaksut/branches/atsoukka/trunk/getpaid/verkkomaksut/verkkomaksut.py
+++ getpaid.verkkomaksut/branches/atsoukka/trunk/getpaid/verkkomaksut/verkkomaksut.py
@@ -11,11 +11,9 @@ from persistent import Persistent
 from zope import schema, interface
 
 from getpaid.core import interfaces
+from getpaid.core.interfaces import workflow_states as wf
 
 from getpaid.verkkomaksut.interfaces import IVerkkomaksutProcessor, IVerkkomaksutOptions
-
-from urllib import urlencode
-from urllib2 import urlopen, Request
 
 
 class VerkkomaksutProcessor(Persistent):
@@ -30,15 +28,25 @@ class VerkkomaksutProcessor(Persistent):
         super(VerkkomaksutProcessor, self).__init__()
 
     def authorize(self, order, payment):
-        # authorize only if order already paid through button
-        # otherwise, 
-        #   return interfaces.keys.results_async
-        import pdb; pdb.set_trace()
-        return "Authorization failed"
+        # authorization only through payment button
+        if order.finance_state == wf.order.finance.CHARGED:
+            # return normal "success" if charging was already done
+            return interfaces.keys.results_success
+        # I dislike the idea of returning "interfaces.keys.results_async" here,
+        # because even this is an offsite processor, this can't really do anything
+        # asynchronously here, but only through user clicking the payment button.
+        return _(u"Authorization failed")
 
     def capture(self, order, price):
-        # always returns async - just here to make the processor happy
-        return interfaces.keys.results_async
+        # capture only with authorization through payment button
+        if order.finance_state == wf.order.finance.CHARGED:
+            # return normal "success" if charging was already done
+            return interfaces.keys.results_success
+        # I dislike the idea of returning "interfaces.keys.results_async" here,
+        # because even this is an offsite processor, this can't really do anything
+        # asynchronously here, but only through user clicking the payment button.
+        return _(u"Capture failed")
 
     def refund(self, order, amount):
-        return "Refund failed"
+        # refund not supported
+        return _(u"Refund failed")
