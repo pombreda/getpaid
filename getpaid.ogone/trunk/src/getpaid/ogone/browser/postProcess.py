4098bd7853c94f581ac5215f36de8e2a81a3dcd7
do3ccQRV@gmail.com <do3ccQRV@gmail.com@5c657d5f-f92f-0410-bf4e-417a11dd3c0b>
2011-12-02 12:04:54 +0000

    Add logging to post processing, only trigger workflow transition to paid, if ogone confirms payment, and not on everything, like, errors or cancellations

--- getpaid.ogone/trunk/src/getpaid/ogone/browser/postProcess.py
+++ getpaid.ogone/trunk/src/getpaid/ogone/browser/postProcess.py
@@ -1,9 +1,12 @@
 from Products.Five import BrowserView
 from getpaid.core.interfaces import IOrderManager
 from getpaid.ogone.interfaces import IOgoneStandardOptions
+from logging import getLogger
+from pprint import pformat
 from zope.component import getUtility
 import sha
 
+logger = getLogger('getpaid.ogone')
 
 class ValidatePaymentParameters(object):
 
@@ -14,22 +17,16 @@ class ValidatePaymentParameters(object):
         """
         options = IOgoneStandardOptions(self.context)
         shaPassword = options.shaout
-        orderID = self.request.get('orderID')
-        currency = self.request.get('currency')
-        amount = self.request.get('amount')
-        pm = self.request.get('PM')
-        acceptance = self.request.get('ACCEPTANCE')
-        status = self.request.get('STATUS')
-        cardno = self.request.get('CARDNO')
-        payid = self.request.get('PAYID')
-        ncerror = self.request.get('NCERROR')
-        brand = self.request.get('BRAND')
+        checkdata = {}
+        [checkdata.update({x.upper(): self.request.form[x]})
+         for x in self.request.form.keys()]
+        keys = checkdata.keys()
+        keys.sort()
         shaObject = sha.new()
-        shaObject.update('%s%s%s%s%s%s%s%s%s%s%s' % (orderID, currency,
-                                                     amount, pm, acceptance,
-                                                     status, cardno, payid,
-                                                     ncerror, brand,
-                                                     shaPassword))
+        for key in keys:
+            if checkdata[key] and key not in ['SHASIGN']:
+                shaObject.update(("%s=%s%s" % (key, checkdata[key],
+                                               shaPassword)))
         hexString = shaObject.hexdigest()
         return hexString.upper()
 
@@ -53,13 +50,23 @@ class OgonePostProcessAccepted(BrowserView, ValidatePaymentParameters):
         We update the order information and returns the template
         """
         if not self.validate():
-            raise 'Incorrect SHA OUT Signature'
+            raise Exception('Incorrect SHA OUT Signature')
+    # According to ogone manual, status codes starting with 5 or 9 mean
+    # something good
+	if self.request.form['STATUS'][0] in '59':
+	    log_cmd = logger.info
+	else:
+	    log_cmd = logger.warn
+	log_cmd("Received async order update information: " +\
+  	    pformat(self.request.form))
         orderId = self.request.orderID
         options = IOgoneStandardOptions(self.context)
         currency = options.currency
         orderManager = getUtility(IOrderManager)
         order = orderManager.get(orderId)
-        order.finance_workflow.fireTransition("charge-charging")
+    # According to ogone manual, status code '9' means paid
+	if self.request.form['STATUS'] == '9':
+            order.finance_workflow.fireTransition("charge-charging")
         return 1
 
 
